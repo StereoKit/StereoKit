4436531e92cbc94f7a7ce27ba8fc124bc4fed8f0
Nick Klingensmith <programmerpichu@gmail.com>
2019-07-26 09:23:15 -0700

    -Updated C# bindings for new material API changes!

--- StereoKit/Material.cs
+++ StereoKit/Material.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StereoKit
 {
@@ -11,15 +7,27 @@ namespace StereoKit
     {
         #region Imports
         [DllImport(Util.DllName)]
-        static extern IntPtr material_create(IntPtr shader, IntPtr texture);
+        static extern IntPtr material_create(IntPtr shader);
         [DllImport(Util.DllName)]
         static extern void material_release(IntPtr material);
+        [DllImport(Util.DllName, CharSet = CharSet.Ansi)]
+        static extern void material_set_float  (IntPtr material, string name, float   value);
+        [DllImport(Util.DllName, CharSet = CharSet.Ansi)]
+        static extern void material_set_color32(IntPtr material, string name, Color32 value);
+        [DllImport(Util.DllName, CharSet = CharSet.Ansi)]
+        static extern void material_set_color  (IntPtr material, string name, Color   value);
+        [DllImport(Util.DllName, CharSet = CharSet.Ansi)]
+        static extern void material_set_vector (IntPtr material, string name, Vec4    value);
+        [DllImport(Util.DllName, CharSet = CharSet.Ansi)]
+        static extern void material_set_matrix (IntPtr material, string name, Matrix  value);
+        [DllImport(Util.DllName, CharSet = CharSet.Ansi)]
+        static extern void material_set_texture(IntPtr material, string name, IntPtr  value);
         #endregion
 
         internal IntPtr _materialInst;
-        public Material(Shader shader, Tex2D texture)
+        public Material(Shader shader)
         {
-            _materialInst = material_create(shader._shaderInst, texture._texInst);
+            _materialInst = material_create(shader._shaderInst);
             if (_materialInst == IntPtr.Zero)
                 Console.WriteLine("Couldn't create material!");
         }
@@ -27,6 +35,31 @@ namespace StereoKit
         {
             if (_materialInst != IntPtr.Zero)
                 material_release(_materialInst);
+        }
+
+        public void SetFloat(string name, float value)
+        {
+            material_set_float(_materialInst, name, value);
+        }
+        public void SetColor(string name, Color32 value)
+        {
+            material_set_color32(_materialInst, name, value);
+        }
+        public void SetColor(string name, Color value)
+        {
+            material_set_color(_materialInst, name, value);
+        }
+        public void SetVector(string name, Vec4 value)
+        {
+            material_set_vector(_materialInst, name, value);
+        }
+        public void SetMatrix(string name, float value)
+        {
+            material_set_float(_materialInst, name, value);
+        }
+        public void SetTexture(string name, Tex2D value)
+        {
+            material_set_texture(_materialInst, name, value._texInst);
         }
     }
 }
