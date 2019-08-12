ed054bfc5ecefdc9e9c2b81cb807bff1c5777fa3
Nick Klingensmith <programmerpichu@gmail.com>
2019-08-12 09:34:36 -0700

    -Added procedural sphere mesh generation function.

--- StereoKitC/stereokit.h
+++ StereoKitC/stereokit.h
@@ -134,7 +134,8 @@ SK_API void   mesh_release  (mesh_t mesh);
 SK_API void   mesh_set_verts(mesh_t mesh, vert_t   *vertices, int32_t vertex_count);
 SK_API void   mesh_set_inds (mesh_t mesh, uint16_t *indices,  int32_t index_count);
 
-SK_API mesh_t mesh_gen_cube (const char *id, vec3 dimensions, int32_t subdivisions);
+SK_API mesh_t mesh_gen_cube  (const char *id, vec3 dimensions, int32_t subdivisions = 0);
+SK_API mesh_t mesh_gen_sphere(const char *id, float diameter,  int32_t subdivisions = 4);
 
 ///////////////////////////////////////////
 
