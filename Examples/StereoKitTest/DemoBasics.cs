c110a1c7f22d0129a8fa2a7ede3234b1791eee4e
Nick Klingensmith <programmerpichu@gmail.com>
2019-08-23 16:21:50 -0700

    -Adding a little scaffolding for multi-example scenes in the C# app.

---
+++ Examples/StereoKitTest/DemoBasics.cs
@@ -0,0 +1,55 @@
+﻿using StereoKit;
+using System.Collections.Generic;
+using System.Linq;
+
+class DemoBasics : ISKApp
+{
+    Model cube;
+    Model gltf;
+    Transform solidTr;
+    Transform floorTr;
+    Solid floorSolid;
+
+    List<Solid> objects = new List<Solid>();
+
+    public void Initialize()
+    {
+        string root = "../Examples/Assets/";
+        Tex2D cubemap = Tex2D.FromEquirectangular(root + "Sky/sky.hdr");
+        Renderer.SetSkytex(cubemap);
+
+        Material floorMat = new Material(Shader.Find("default/shader_pbr"));
+        floorMat["diffuse"  ] = new Tex2D(root + "test.png");
+        floorMat["normal"   ] = new Tex2D(root + "test_normal.png");
+        floorMat["tex_scale"] = 6;
+
+        cube = new Model(Mesh.GenerateCube("app/mesh_cube", Vec3.One), floorMat);
+        gltf = new Model(root + "DamagedHelmet.gltf");
+        floorTr = new Transform(new Vec3(0, -1.5f, 0), new Vec3(5, 1, 5));
+        solidTr = new Transform(Vec3.Zero, Vec3.One * 0.25f);
+
+        floorSolid = new Solid(floorTr.Position, floorTr.Rotation, SolidType.Immovable);
+        floorSolid.AddBox(floorTr.Scale);
+    }
+
+    public void Update()
+    {
+        if (Input.Hand(Handed.Right).IsJustPinched)
+        {
+            objects.Add(new Solid(new Vec3(0, 3, 0), Quat.Identity));
+            objects.Last().AddSphere(.45f, 40);
+            objects.Last().AddBox(Vec3.One * .35f, 40);
+        }
+
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].GetTransform(ref solidTr);
+            Renderer.Add(gltf, solidTr);
+        }
+        Renderer.Add(cube, floorTr);
+    }
+
+    public void Shutdown()
+    {
+    }
+}
--- Examples/StereoKitTest/Program.cs
+++ Examples/StereoKitTest/Program.cs
@@ -1,49 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using StereoKit;
 
 class Program 
 {
+    static ISKApp activeApp;
+    static ISKApp nextApp;
+    public static ISKApp ActiveApp { get{ return activeApp;} set { nextApp = value; } }
     static void Main(string[] args) 
     {
         if (!StereoKitApp.Initialize("StereoKit C#", Runtime.Flatscreen, true))
             Environment.Exit(1);
 
-        string root = "../Examples/Assets/";
-        Tex2D cubemap = Tex2D.FromEquirectangular(root + "Sky/sky.hdr");
-        Renderer.SetSkytex(cubemap);
+        activeApp = new DemoBasics();
+        activeApp.Initialize();
 
-        Material floorMat = new Material(Shader.Find("default/shader_pbr"));
-        floorMat["diffuse"  ] = new Tex2D(root + "test.png");
-        floorMat["normal"   ] = new Tex2D(root + "test_normal.png");
-        floorMat["tex_scale"] = 6;
-
-        Model     cube   = new Model(Mesh.GenerateCube("app/mesh_cube", Vec3.One), floorMat);
-        Model     gltf   = new Model(root+"DamagedHelmet.gltf");
-        Transform floorTr= new Transform(new Vec3(0,-1.5f, 0), new Vec3(5,1,5));
-        Transform solidTr= new Transform(Vec3.Zero, Vec3.One * 0.25f);
-
-        Solid floorSolid = new Solid(floorTr.Position, floorTr.Rotation, SolidType.Immovable);
-        floorSolid.AddBox(floorTr.Scale);
-        
-        List<Solid> objects = new List<Solid>();
         while (StereoKitApp.Step(() =>
         {
-            if (Input.Hand(Handed.Right).IsJustPinched)
+            activeApp.Update();
+            if (nextApp != null)
             {
-                objects.Add(new Solid(new Vec3(0,3,0), Quat.Identity));
-                objects.Last().AddSphere(.45f, 40);
-                objects.Last().AddBox   (Vec3.One*.35f, 40);
+                activeApp.Shutdown();
+                nextApp.Initialize();
+                activeApp = nextApp;
+                nextApp = null;
             }
-
-            for (int i = 0; i < objects.Count; i++)
-            {
-                objects[i].GetTransform(ref solidTr);
-                Renderer.Add(gltf, solidTr);
-            }
-            Renderer.Add(cube, floorTr);
         }));
+
+        activeApp.Shutdown();
 
         StereoKitApp.Shutdown();
     }
