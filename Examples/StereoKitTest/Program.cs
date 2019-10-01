f3f3d8431fc02521ef6e1a528f59272805415005
Nick Klingensmith <programmerpichu@gmail.com>
2019-10-01 16:45:40 -0700

    -Cleaning up C# demos a bit.

--- Examples/StereoKitTest/Program.cs
+++ Examples/StereoKitTest/Program.cs
@@ -3,6 +3,12 @@ using StereoKit;
 
 class Program 
 {
+    public const string Root = "../Examples/Assets/";
+
+    static Model     floorMesh;
+    static Transform floorTr;
+    static Solid     floorSolid;
+
     static IScene activeScene;
     static IScene nextScene;
     public static IScene ActiveScene { get{ return activeScene;} set { nextScene = value; } }
@@ -10,6 +16,7 @@ class Program
     {
         if (!StereoKitApp.Initialize("StereoKit C#", Runtime.Flatscreen, true))
             Environment.Exit(1);
+        CommonInit();
 
         activeScene = new DemoUI();
         activeScene.Initialize();
@@ -23,11 +30,37 @@ class Program
                 activeScene = nextScene;
                 nextScene = null;
             }
+            CommonUpdate();
             activeScene.Update();
         }));
 
         activeScene.Shutdown();
+        CommonShutdown();
 
         StereoKitApp.Shutdown();
+    }
+
+    static void CommonInit()
+    {
+        Tex2D cubemap = Tex2D.FromEquirectangular(Program.Root + "Sky/sky.hdr");
+        Renderer.SetSkytex(cubemap);
+
+        Material floorMat = new Material(Shader.Find("default/shader_pbr"));
+        floorMat["diffuse"  ] = new Tex2D(Program.Root + "test.png");
+        floorMat["normal"   ] = new Tex2D(Program.Root + "test_normal.png");
+        floorMat["tex_scale"] = 6;
+
+        floorMesh = new Model(Mesh.GenerateCube("app/mesh_cube", Vec3.One), floorMat);
+        floorTr   = new Transform(new Vec3(0, -1.5f, 0), new Vec3(5, 1, 5));
+
+        floorSolid = new Solid(floorTr.Position, floorTr.Rotation, SolidType.Immovable);
+        floorSolid.AddBox(floorTr.Scale);
+    }
+    static void CommonUpdate()
+    {
+        Renderer.Add(floorMesh, floorTr);
+    }
+    static void CommonShutdown()
+    {
     }
 }
