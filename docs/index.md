23d1bb847a2eec990ccd33994e0582267ba58314
Nick Klingensmith <programmerpichu@gmail.com>
2019-10-31 17:07:19 -0700

    -Initial template for docs page!

---
+++ docs/index.md
@@ -0,0 +1,23 @@
+<script src="/pages/data.js"></script>
+<script src="/src/docs.js"></script>
+<link rel="stylesheet" type="text/css" href="src/style.css" />
+ 
+<header id="header">
+    <img src="/img/StereoKitWideLight.svg" alt="StereoKit" height="100%"/>
+</header>
+
+<div id="search-bar">
+    <input type="text" id="search" oninput="updateSearch(this.value)" placeholder="Search.."> 
+</div>
+
+<div id="breadcrumbs">
+    <a href="">Root</a> / <a href="">StereoKit</a>
+</div>
+
+<nav id="nav">
+    <div id="links" class="tree-view"></div>
+</nav>
+
+<main>
+    <p>Content goes here.</p>
+</main>
---
+++ docs/src/docs.js
@@ -0,0 +1,58 @@
+var search = "";
+
+function updateSearch(text) {
+    search = text.toLowerCase();
+    
+    // Check each leaf to see if it's visible
+    var leaves = document.getElementsByClassName('tree-leaf');
+    for(var i=0; i<leaves.length; i+=1) {
+        var valid = nameValid(leaves[i].textContent);
+        leaves[i].style.display = valid ? "block" : "none"; 
+    }
+
+    // See if a branch has any visible leaves
+    var branches = document.getElementsByClassName('tree-branch');
+    for(var i=0; i<branches.length; i+=1) {
+        var leaves = branches[i].getElementsByClassName('tree-leaf');
+        var valid  = false;
+        for (var f=0; f<leaves.length; f+=1) {
+            if (leaves[f].style.display === "block") {
+                valid = true;
+            }
+        } 
+        branches[i].style.display = valid ? "block" : "none"; 
+    }
+ }
+function nameValid(name) {
+    return search === "" || name.toLowerCase().includes(search);
+    
+}
+function renderFolder(folder, parent) {
+    var result = parent == null ? "<ul>" : "<li class='tree-branch'><input type='checkbox'/><span></span>"+folder.name+"<ul>";
+    parent = parent == null ? "" : parent + folder.name;
+
+    // Render the pages in this folder
+    if (folder.pages) {
+        for (var i = 0; i<folder.pages.length; i+=1) {
+            result += "<li class='tree-leaf'><a href=''>"+folder.pages[i]+"</a></li>";
+        }
+    }
+
+    // Render sub-folders
+    if (folder.folders) {
+        for (var i = 0; i<folder.folders.length; i+=1) {
+            var folderText = renderFolder(folder.folders[i], parent);
+            result += folderText;
+        }
+    }
+    result += "</ul></li>";
+    return result;
+}
+function renderNav() {
+    var nav = document.getElementById("links");
+    nav.innerHTML = renderFolder(documents);
+}
+
+window.addEventListener('load', function () {
+    renderNav();
+});
