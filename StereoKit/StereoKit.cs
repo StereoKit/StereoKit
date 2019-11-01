92cde17f8c21cdfb70a08b4f867ce34d2c2095ef
Nick Klingensmith <programmerpichu@gmail.com>
2019-10-31 21:45:40 -0700

    -Added a project for converting CommentDoc .xml files into .md files for the doc site. Just some early parts!

--- StereoKit/StereoKit.cs
+++ StereoKit/StereoKit.cs
@@ -10,7 +10,12 @@ namespace StereoKit
         public static Settings settings;
         public static bool     IsInitialized { get; private set; }
         public static Runtime  ActiveRuntime { get { return NativeAPI.sk_active_runtime(); } }
-        
+
+        /// <summary>Initializes StereoKit window, default resources, systems, etc. Set settings before calling this function, if defaults need changed!</summary>
+        /// <param name="name">Name of the application, this shows up an the top of the Win32 window, and is submitted to OpenXR. OpenXR caps this at 128 characters.</param>
+        /// <param name="runtimePreference">Which runtime should we try to load?</param>
+        /// <param name="fallback">If the preferred runtime fails, should we fall back to flatscreen?</param>
+        /// <returns>Returns true if all systems are successfully initialized!</returns>
         public static bool Initialize(string name, Runtime runtimePreference, bool fallback = true)
         {
             // Selects appropriate DLL for the platform, must be called first, and before
@@ -31,6 +36,9 @@ namespace StereoKit
             return NativeAPI.sk_init(name, runtime, fallback?1:0) > 0;
         }
 
+        /// <summary>
+        /// Shuts down all StereoKit initialized systems. Release your own StereoKit created assets before calling this.
+        /// </summary>
         public static void Shutdown()
         {
             if (IsInitialized)
@@ -40,6 +48,11 @@ namespace StereoKit
             }
         }
 
+        /// <summary>
+        /// Steps all StereoKit systems, and inserts user code via callback between the appropriate system updates.
+        /// </summary>
+        /// <param name="onStep">A callback where you put your application code! This gets called between StereoKit systems, after frame setup, but before render.</param>
+        /// <returns>If an exit message is received from the platform, this function will return false.</returns>
         public static bool Step(Action onStep)
         {
             return NativeAPI.sk_step(onStep) > 0;
---
+++ StereoKitDocumenter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml;
+
+namespace StereoKitDocumenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlDocs = "../../../bin/StereoKit.xml";
+
+            XmlReader reader = XmlReader.Create(xmlDocs);
+            while(reader.ReadToFollowing("member"))
+            {
+                string name = reader.GetAttribute("name");
+                XmlReader member = reader.ReadSubtree();
+
+                XmlReader summary = member;
+                summary.ReadToFollowing("summary");
+                
+                Console.WriteLine(name);
+                Console.WriteLine(summary.ReadElementContentAsString().Trim());
+            }
+        }
+    }
+}
