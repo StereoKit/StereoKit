ebb287150de006cd5eac74b4a98d93fa492e79ab
Nick Klingensmith <programmerpichu@gmail.com>
2019-11-01 12:14:23 -0700

    -Added a UWP project to the StereoKit templates!

---
+++ StereoKitTemplates/SKTemplate_UWP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StereoKit;
+
+namespace SKTemplate_UWP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!StereoKitApp.Initialize("StereoKit C#", Runtime.MixedReality, true))
+                Environment.Exit(1);
+
+            Model cube = new Model(Mesh.GenerateRoundedCube(Vec3.One, 0.1f), Material.Find("default/material"));
+
+            while (StereoKitApp.Step(() =>
+            {
+                Renderer.Add(cube, Matrix.Identity, Color.White);
+            }));
+
+            StereoKitApp.Shutdown();
+        }
+    }
+}
