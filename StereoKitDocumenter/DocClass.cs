c591137f39cfb1d44b6e3964158e0dc936f74e71
Nick Klingensmith <programmerpichu@gmail.com>
2019-11-01 09:09:33 -0700

    -Documenter now generates basic doc files for each method and type!

---
+++ StereoKitDocumenter/DocClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StereoKitDocumenter
+{
+    class DocClass
+    {
+        public string name;
+        public string summary;
+
+        public string FileName { get{ 
+            return Path.Combine( Program.referenceOut, name+".md");
+        } }
+        public override string ToString()
+        {
+
+            return $@"
+---
+layout: default
+title: {name}
+description: {summary}
+---
+** {name}
+___
+{summary}
+";
+
+        }
+    }
+}
---
+++ StereoKitDocumenter/DocMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StereoKitDocumenter
+{
+    class DocMethod
+    {
+        public string name;
+        public string className;
+        public string summary;
+        public string returns;
+
+        public string FileName { get{ 
+            return Path.Combine( Program.referenceOut, name+".md");
+        } }
+
+        public override string ToString()
+        {
+            
+            return $@"
+---
+layout: default
+title: {className}.{name}
+description: {summary}
+---
+** {className}.{name}
+___
+{summary}
+
+{returns}
+";
+
+        }
+    }
+}
