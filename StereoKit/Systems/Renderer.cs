750be00b80671358e2f8b06d5a176bd337bff806
Nick Klingensmith <programmerpichu@gmail.com>
2019-11-11 21:10:17 -0800

    -Some tweaks to make screenshots look better!

--- StereoKit/Systems/Renderer.cs
+++ StereoKit/Systems/Renderer.cs
@@ -1,6 +1,10 @@
 ﻿
 namespace StereoKit
 {
+    /// <summary>Do you need to draw something? Well, you're probably in the right place!
+    /// This static class includes a variety of different drawing methods, from rendering 
+    /// Models and Meshes, to setting rendering options and drawing to offscreen surfaces!
+    /// Even better, it's entirely a static class, so you can call it from anywhere :)</summary>
     public class Renderer
     {
         public static void Add(Mesh mesh, Material material, Matrix transform)
