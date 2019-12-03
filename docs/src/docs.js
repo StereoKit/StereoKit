f8f78d9449737ac85a5e9285fbeb41a982a8f519
Nick Klingensmith <programmerpichu@gmail.com>
2019-12-03 14:41:53 -0800

    -Docs page can now remember search terms and tree hierarchy through page loads. -Some docs analytics! Should help spot what people need help with.

--- docs/src/docs.js
+++ docs/src/docs.js
@@ -3,35 +3,64 @@
 
 var search = "";
 
+var treeDict = { data:{} };
+treeDict.hasItem = function (name) {
+    return treeDict.data[name] > 0;
+}
+treeDict.setItem = function(name, val) {
+    if (val)
+        treeDict.data[name] = 1;
+    else
+        delete treeDict.data[name];
+        
+    sessionStorage.setItem("treeDict", Object.keys(treeDict.data).join());
+}
+treeDict.load = function() {
+    treeDict.data = {};
+    var treeData  = sessionStorage.getItem("treeDict");
+    var treeItems = treeData == null ? [] : treeData.split(",");
+    for (var i=0; i<treeItems.length; i++) {
+        treeDict.data[treeItems[i]] = 1;
+    }
+
+    if (treeData == null) {
+        treeDict.data["Guides"] = 1;
+        treeDict.data["Reference"] = 1;
+    }
+}
+
 function updateSearch(text) {
+    sessionStorage.setItem("search", text);
     search = text.toLowerCase();
     
     // Check each leaf to see if it's visible
-    var leaves = document.getElementsByClassName('tree-leaf');
+    var leaves = document.getElementsByClassName("tree-leaf");
     for(var i=0; i<leaves.length; i+=1) {
         var valid = nameValid(leaves[i].textContent);
-        leaves[i].style.display = valid ? "block" : "none"; 
+        leaves[i].style.display = valid ? "block" : "none";
     }
 
     // See if a branch has any visible leaves
-    var branches = document.getElementsByClassName('tree-branch');
+    var branches = document.getElementsByClassName("tree-branch");
     for(var i=0; i<branches.length; i+=1) {
-        var leaves = branches[i].getElementsByClassName('tree-leaf');
+        var leaves = branches[i].getElementsByClassName("tree-leaf");
         var valid  = false;
         for (var f=0; f<leaves.length; f+=1) {
             if (leaves[f].style.display === "block") {
                 valid = true;
             }
-        } 
-        branches[i].style.display = valid ? "block" : "none"; 
+        }
+
+        branches[i].style.display = valid ? "block" : "none";
+
+        branches[i].getElementsByClassName("tree-branch-check")[0].checked = !(text !== "" || treeDict.hasItem(branches[i].attributes.branchname.value));
     }
 }
 function nameValid(name) {
     return search === "" || name.toLowerCase().includes(search);
-    
 }
 function renderFolder(folder, parent) {
-    var result = parent == "" ? "<ul>" : "<li class='tree-branch'><input type='checkbox' "+(parent=="Pages"?"":"checked")+" /><span></span><a href='{{site.url}}/"+parent+"/"+folder.name.replace(" ", "-")+".html'>"+folder.name+"</a><ul>";
+    var result = parent == "" ? "<ul>" : "<li class='tree-branch' branchName='"+folder.name+"'><input class='tree-branch-check' type='checkbox' onClick='treeDict.setItem(\""+folder.name+"\", !this.checked)' /><span></span><a href='{{site.url}}/"+parent+"/"+folder.name.replace(" ", "-")+".html'>"+folder.name+"</a><ul>";
     if (parent != "") parent += "/";
     parent += folder.name.replace(" ", "-");
 
@@ -57,6 +86,12 @@ function renderNav() {
     nav.innerHTML = renderFolder(documents, "");
 }
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
     renderNav();
+
+    treeDict.load();
+    
+    var searchBar = document.getElementById("search");
+    searchBar.value = sessionStorage.getItem("search");
+    updateSearch(searchBar.value);
 });
