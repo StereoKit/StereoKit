c75fe649ee8e75a8d70f33abb28fde424fe2b55a
Nick Klingensmith <programmerpichu@gmail.com>
2019-12-18 22:16:39 -0800

    -Made the getting started guide shorter!

--- Examples/StereoKitTest/Guides/GuideGettingStarted.cs
+++ Examples/StereoKitTest/Guides/GuideGettingStarted.cs
@@ -5,24 +5,13 @@
 /// 
 /// Before we get started, you'll need some software! You may have some of this already, but here's the list!
 /// 
-/// [Visual Studio 2019](https://visualstudio.microsoft.com/vs/) - the Community version is great! But it doesn't
-/// matter which one, so long as it's not VS 2017. Visual Studio Code also won't work, that's a totally
-/// separate product :)
-/// 
-/// This also comes with some extra configuration! While installing Visual Studio, you'll need to pick a
-/// few workloads. Try '.NET Desktop development', 'Universal Windows Platform development' and '.NET Core 
-/// cross-platform development'. That should cover it, but I may refine this list later on!
-/// 
-/// [OpenXR Runtime](https://www.microsoft.com/store/productId/9n5cvvl23qbt) - StereoKit is powered by OpenXR! You'll
-/// want this installed and enabled on any device running your application! If you're deploying to a HoloLens 2, this
-/// may already be pre-installed there, just make sure it's up-to-date.
-/// 
-/// ## Setup
-/// 
-/// The recommended way to set up StereoKit is through the [Visual Studio Template](https://github.com/maluoi/StereoKit/raw/master/Documentation/SKTemplates.vsix)!
-/// This is a really easy way to get set up, and will have you started in no time! For existing projects or 
-/// more advanced users, you can directly use the [NuGet package](https://www.nuget.org/packages/StereoKit) that's used
-/// by the template.
+/// - [Visual Studio 2019](https://visualstudio.microsoft.com/vs/) - the Community version is great! Use these workloads:
+///   * .NET Desktop development
+///   * Universal Windows Platform development
+///   * .NET Core cross-platform development
+/// - [OpenXR Runtime] (https://www.microsoft.com/store/productId/9n5cvvl23qbt) - Install *and enable* this on your desktop and device!
+/// - [StereoKit's Visual Studio Template](https://github.com/maluoi/StereoKit/raw/master/Documentation/SKTemplates.vsix) - The fastest way to set up a StereoKit project!
+///   * You can also directly install the [NuGet package] (https://www.nuget.org/packages/StereoKit) into your project of choice.
 /// 
 /// The source for StereoKit is [available on Github](https://github.com/maluoi/StereoKit) if you're interested in peeking
 /// at the code or modifying it yourself, but it's a bit more complex to dive into! If you're just getting started 
@@ -30,9 +19,11 @@
 /// 
 /// ## The Template
 /// 
+/// ![Create New Project]({{site.url}}/img/screenshots/VSNewProject.png)
+/// 
 /// There are two templates to pick from! One is for UWP, and one is for .NET Core. Choose the UWP one if you're deploying
 /// to HoloLens 2, and choose the .NET Core one if you're doing development on a Windows Mixed Reality headset! When using
-/// UWP, .Net Native is used for Release builds and all ARM64 builds, so it can be faster to iterate in Debug/x64!
+/// UWP, iterating is fastest with Debug/x64, using desktop VR or the flatscreen mode.
 /// 
 /// The starting code is pretty simple! Initialize StereoKit in Mixed Reality mode, create a 3D model that's a rounded
 /// cube with a default material, and then draw that model every step of the application!
