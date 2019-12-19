812637c72bd3b82e5988d5ae33103cfb0b2727b2
Nick Klingensmith <programmerpichu@gmail.com>
2019-12-19 12:22:39 -0800

    -Tweaks to getting started guide

--- Examples/StereoKitTest/Guides/GuideGettingStarted.cs
+++ Examples/StereoKitTest/Guides/GuideGettingStarted.cs
@@ -6,12 +6,12 @@
 /// Before we get started, you'll need some software! You may have some of this already, but here's the list!
 /// 
 /// - [Visual Studio 2019](https://visualstudio.microsoft.com/vs/) - the Community version is great! Use these workloads:
-///   * .NET Desktop development
-///   * Universal Windows Platform development
-///   * .NET Core cross-platform development
-/// - [OpenXR Runtime] (https://www.microsoft.com/store/productId/9n5cvvl23qbt) - Install *and enable* this on your desktop and device!
+/// &nbsp;&nbsp;* .NET Desktop development
+/// &nbsp;&nbsp;* Universal Windows Platform development
+/// &nbsp;&nbsp;* .NET Core cross-platform development
+/// - [OpenXR Runtime](https://www.microsoft.com/store/productId/9n5cvvl23qbt) - Install *and enable* this on your desktop and device!
 /// - [StereoKit's Visual Studio Template](https://github.com/maluoi/StereoKit/raw/master/Documentation/SKTemplates.vsix) - The fastest way to set up a StereoKit project!
-///   * You can also directly install the [NuGet package] (https://www.nuget.org/packages/StereoKit) into your project of choice.
+/// &nbsp;&nbsp;* You can also directly install the [NuGet package] (https://www.nuget.org/packages/StereoKit) into your project of choice.
 /// 
 /// The source for StereoKit is [available on Github](https://github.com/maluoi/StereoKit) if you're interested in peeking
 /// at the code or modifying it yourself, but it's a bit more complex to dive into! If you're just getting started 
@@ -33,13 +33,13 @@
 using System;
 using StereoKit;
 
-namespace SKYourTemplate
+namespace Project
 {
     class Program
     {
         static void Main(string[] args)
         {
-            if (!StereoKitApp.Initialize("SKYourTemplate", Runtime.MixedReality))
+            if (!StereoKitApp.Initialize("Project", Runtime.MixedReality))
                 Environment.Exit(1);
 
             Model cube = Model.FromMesh(
