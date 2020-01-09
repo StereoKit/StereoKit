d4f73121dd8f55939862be88893c2438280de14c
Nick Klingensmith <programmerpichu@gmail.com>
2020-01-08 23:14:05 -0800

    -Perfecting line painting

--- Examples/StereoKitTest/DemoLines.cs
+++ Examples/StereoKitTest/DemoLines.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using StereoKit;
+using System;
 using System.Collections.Generic;
 
 namespace StereoKitTest
@@ -35,30 +36,39 @@ namespace StereoKitTest
             Draw(Handed.Right);
         }
 
+        Vec3 prevTip;
         List<LinePoint> drawPoints = new List<LinePoint>();
         void Draw(Handed handed)
         {
             Hand hand = Input.Hand(handed);
             Vec3 tip  = hand[FingerId.Index, JointId.Tip].position;
+            tip = prevTip + (tip-prevTip) * 0.3f;
             const float size    = 0.03f;
             const float minDist = 2 * Units.cm2m;
 
             if (hand.IsJustPinched) { 
                 drawPoints.Clear();
                 drawPoints.Add(new LinePoint(tip, Color.White, size));
+                drawPoints.Add(new LinePoint(tip, Color.White, size));
+                prevTip = tip;
             }
 
             if (hand.IsPinched)
             {
-                Vec3  prev = drawPoints[drawPoints.Count-1].pt;
-                float dist = (prev-tip).Magnitude;// Vec3.Distance(prev, tip);
-                Log.Info(dist.ToString());
-                if (dist > minDist) { 
-                    drawPoints.Add(new LinePoint(tip, Color.White, (minDist/dist) * size));
+                Vec3  prev  = drawPoints[drawPoints.Count - 2].pt;
+                Vec3  dir   = (prev - (drawPoints.Count > 2 ? drawPoints[drawPoints.Count - 3].pt : drawPoints[drawPoints.Count - 1].pt)).Normalized();
+                float dist  = (prev - tip).Magnitude;
+                float speed = (tip - prevTip).Magnitude * Time.Elapsedf;
+                LinePoint here = new LinePoint(tip, Color.White, Math.Max(1-speed/0.0003f,0.1f) * size);
+                drawPoints[drawPoints.Count - 1]  = here;
+                
+                if ((Vec3.Dot( dir, (tip-prev).Normalized() ) < 0.99f && dist > 0.005f) || dist > 0.3f) { 
+                    drawPoints.Add(here);
                 }
             }
 
             Lines.Add(drawPoints.ToArray());
+            prevTip = tip;
         }
     }
 }
