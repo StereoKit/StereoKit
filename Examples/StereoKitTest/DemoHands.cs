610820afc10211593ecee1b8b5e0354cbc1d2c2c
Nick Klingensmith <programmerpichu@gmail.com>
2020-01-22 16:29:25 -0800

    -Radial hand menu as a re-usable IStepper

--- Examples/StereoKitTest/DemoHands.cs
+++ Examples/StereoKitTest/DemoHands.cs
@@ -10,6 +10,7 @@
 /// :End:
 
 using StereoKit;
+using StereoKit.Framework;
 using System;
 
 namespace StereoKitTest
@@ -23,6 +24,7 @@ namespace StereoKitTest
         bool showPointers = true;
 
         Mesh jointMesh = Mesh.GenerateSphere(1);
+        HandMenuRadial handMenu;
 
         public void Initialize() {
             /// :CodeDoc: Guides Using Hands
@@ -55,9 +57,35 @@ namespace StereoKitTest
             /// These are all convenience functions wrapping the `hand.pinchState` bit-flag, so you
             /// can also use that directly if you want to do some bit-flag wizardry!
             /// :End:
+            
+            handMenu = StereoKitApp.AddStepper(new HandMenuRadial(
+                new HandRadialLayer("Root",
+                    new HandMenuItem("One", null, null, "One"),
+                    new HandMenuItem("Two", null, null, "Two"),
+                    new HandMenuItem("Three", null, null, "Three"),
+                    new HandMenuItem("Four", null, null, "Four"),
+                    new HandMenuItem("Cancel", null, null)),
+                new HandRadialLayer("One", 
+                    new HandMenuItem("a", null, ()=>Log.Info("a")),
+                    new HandMenuItem("b", null, ()=>Log.Info("b")),
+                    new HandMenuItem("back", null, null, HandMenuAction.Back)),
+                new HandRadialLayer("Two",
+                    new HandMenuItem("c", null, ()=>Log.Info("c")),
+                    new HandMenuItem("back", null, null, HandMenuAction.Back)),
+                new HandRadialLayer("Three",
+                    new HandMenuItem("d", null, ()=>Log.Info("d")),
+                    new HandMenuItem("e", null, ()=>Log.Info("e")),
+                    new HandMenuItem("f", null, ()=>Log.Info("f")),
+                    new HandMenuItem("back", null, null, HandMenuAction.Back)),
+                new HandRadialLayer("Four",
+                    new HandMenuItem("g", null, () => Log.Info("g")),
+                    new HandMenuItem("h", null, () => Log.Info("h")),
+                    new HandMenuItem("i", null, () => Log.Info("i")),
+                    new HandMenuItem("j", null, () => Log.Info("j")),
+                    new HandMenuItem("back", null, null, HandMenuAction.Back))));
         }
 
-        public void Shutdown() { }
+        public void Shutdown() { StereoKitApp.RemoveStepper(handMenu); }
 
         public void Update()
         {
@@ -79,8 +107,6 @@ namespace StereoKitTest
 
             DrawHandMenu(Handed.Right);
             DrawHandMenu(Handed.Left);
-
-            HandSubMenu(Handed.Right);
         }
 
         /// :CodeDoc: Guides Using Hands
@@ -177,66 +203,6 @@ namespace StereoKitTest
             }
         }
         /// :End:
-        
-        bool showHandSubmenu = false;
-        int  depth = 0;
-        Pose subMenuPose;
-        public void HandSubMenu(Handed handed)
-        {
-            Hand hand = Input.Hand(handed);
-            
-            if (!showHandSubmenu && hand.IsJustGripped)
-            {
-                subMenuPose = hand[FingerId.Index, JointId.Tip].Pose;
-                subMenuPose.orientation = Quat.LookAt(subMenuPose.position, Input.Head.position);
-                showHandSubmenu = true;
-                depth = 0;
-            }
-
-            if (showHandSubmenu)
-            {
-                const float minDist = 0.03f;
-                const float midDist = 0.065f;
-                const float maxDist = 0.1f;
-                const int   count   = 6;
-                const float step    = 360 / count;
-                const float halfStep = step/2;
-
-                Hierarchy.Push(subMenuPose.ToMatrix());
-                Vec3  fingertip = Hierarchy.ToLocal(hand[FingerId.Index, JointId.Tip].position);
-                float magSq    = fingertip.MagnitudeSq;
-                float angle    = (float)Math.Atan2(fingertip.y, fingertip.x) * Units.rad2deg;
-                bool  onPlane  = fingertip.z > -0.02f && fingertip.z < 0.02f;
-                bool  active   = onPlane && magSq > minDist * minDist;
-                bool  selected = onPlane && magSq > midDist * midDist;
-                bool  unfocus  = magSq > maxDist*maxDist;
-
-                if (angle < 0)
-                    angle += 360;
-                int angleId = (int)(angle / step);
-
-                for (int i = 0; i < count; i++)
-                {
-                    bool highlightText = active && angleId == i;
-                    bool highlightLine = highlightText || (active && (angleId+1)%6 == i);
-                    Vec3 dir = Vec3.AngleXY(i * step);
-                    Lines.Add(dir * minDist, dir * maxDist, highlightLine ? Color.White : Color.White*0.5f, highlightLine?0.002f:0.001f);
-                    Text.Add((i+count*depth).ToString(), Matrix.TRS(Vec3.AngleXY(i * step + halfStep)*midDist, Quat.FromAngles(0, 0, i * step + halfStep - 90), highlightText?1.2f:1), TextAlign.XCenter | TextAlign.YBottom);
-                }
-                Lines.Add(Vec3.Zero, new Vec3(fingertip.x, fingertip.y, 0), Color.White * 0.5f, 0.001f);
-
-                Hierarchy.Pop();
-
-                if (unfocus)
-                    showHandSubmenu = false;
-                if (selected)
-                {
-                    Plane plane = new Plane(subMenuPose.position, subMenuPose.Forward);
-                    subMenuPose.position = plane.Closest( hand[FingerId.Index, JointId.Tip].position );
-                    depth += 1;
-                }
-            }
-        }
     }
 }
 
---
+++ StereoKit/Framework/HandMenuRadial.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StereoKit.Framework
+{
+    public enum HandMenuAction
+    {
+        Layer,
+        Back,
+        Close
+    }
+    public class HandMenuItem
+    {
+        public string name;
+        public Sprite image;
+
+        public HandMenuAction action;
+        public Action callback;
+        public string layerName;
+
+        public HandMenuItem(string name, Sprite image, Action callback, string layerName) 
+            : this(name, image, callback, HandMenuAction.Layer)
+        {
+            this.layerName = layerName;
+        }
+        public HandMenuItem(string name, Sprite image, Action callback, HandMenuAction action = HandMenuAction.Close)
+        {
+            this.name     = name;
+            this.image    = image;
+            this.action   = action;
+            this.callback = callback;
+        }
+    }
+    public class HandRadialLayer
+    {
+        public string layerName;
+        public HandMenuItem[] items;
+
+        public HandRadialLayer(string name, params HandMenuItem[] items)
+        {
+            this.layerName = name;
+            this.items = items;
+        }
+    }
+
+    public class HandMenuRadial : IStepper
+    {
+        bool showHandSubmenu = false;
+        Pose subMenuPose;
+
+        HandRadialLayer[] layers;
+        Stack<int>        navStack = new Stack<int>();
+        int               activeLayer;
+
+        public HandMenuRadial(params HandRadialLayer[] menuLayers)
+        {
+            layers = menuLayers;
+            activeLayer = 0;
+        }
+
+        public void Close()
+        {
+            activeLayer = 0;
+            showHandSubmenu = false;
+            navStack.Clear();
+        }
+
+        public bool Enabled => true;
+
+        public bool Initialize() => true;
+
+        public void Shutdown() {}
+
+        public void Step()
+        {
+            Hand hand = Input.Hand(Handed.Right);
+            
+            if (!showHandSubmenu && hand.IsJustGripped)
+            {
+                subMenuPose = hand[FingerId.Index, JointId.Tip].Pose;
+                subMenuPose.orientation = Quat.LookAt(subMenuPose.position, Input.Head.position);
+                showHandSubmenu = true;
+            }
+
+            if (showHandSubmenu)
+            {
+                HandRadialLayer layer = layers[activeLayer];
+
+                const float minDist = 0.03f;
+                const float midDist = 0.065f;
+                const float maxDist = 0.1f;
+                int   count    = layer.items.Length;
+                float step     = 360 / count;
+                float halfStep = step/2;
+
+                Hierarchy.Push(subMenuPose.ToMatrix());
+                Vec3  fingertip = Hierarchy.ToLocal(hand[FingerId.Index, JointId.Tip].position);
+                float magSq    = fingertip.MagnitudeSq;
+                float angle    = (float)Math.Atan2(fingertip.y, fingertip.x) * Units.rad2deg;
+                bool  onPlane  = fingertip.z > -0.02f && fingertip.z < 0.02f;
+                bool  active   = onPlane && magSq > minDist * minDist;
+                bool  selected = onPlane && magSq > midDist * midDist;
+                bool  unfocus  = magSq > maxDist*maxDist;
+
+                if (angle < 0)
+                    angle += 360;
+                int angleId = (int)(angle / step);
+
+                for (int i = 0; i < count; i++)
+                {
+                    bool highlightText = active && angleId == i;
+                    bool highlightLine = highlightText || (active && (angleId+1)%6 == i);
+                    Vec3 dir = Vec3.AngleXY(i * step);
+                    Lines.Add(dir * minDist, dir * maxDist, highlightLine ? Color.White : Color.White*0.5f, highlightLine?0.002f:0.001f);
+                    Text.Add(layer.items[i].name, Matrix.TRS(Vec3.AngleXY(i * step + halfStep)*midDist, Quat.FromAngles(0, 0, i * step + halfStep - 90), highlightText?1.2f:1), TextAlign.XCenter | TextAlign.YBottom);
+                }
+                Lines.Add(Vec3.Zero, new Vec3(fingertip.x, fingertip.y, 0), Color.White * 0.5f, 0.001f);
+
+                Hierarchy.Pop();
+
+                if (unfocus)
+                    Close();
+
+                if (selected)
+                {
+                    HandMenuItem item = layer.items[angleId];
+                    if (item.action == HandMenuAction.Close)
+                    { 
+                        Close();
+                    }
+                    else if (item.action == HandMenuAction.Layer) 
+                    {
+                        navStack.Push(activeLayer);
+                        activeLayer = Array.FindIndex(layers, l=>l.layerName == item.layerName);
+                        if (activeLayer == -1)
+                            Log.Err($"Couldn't find hand menu layer named {item.layerName}!");
+                    } 
+                    else if (item.action == HandMenuAction.Back) 
+                    {
+                        if (navStack.Count > 0)
+                            activeLayer = navStack.Pop();
+                    }
+
+                    if (item.action == HandMenuAction.Back || item.action == HandMenuAction.Layer)
+                    {
+                        Plane plane = new Plane(subMenuPose.position, subMenuPose.Forward);
+                        subMenuPose.position = plane.Closest(hand[FingerId.Index, JointId.Tip].position);
+                    }
+
+                    item.callback?.Invoke();
+                }
+            }
+        }
+    }
+}
