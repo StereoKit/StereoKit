4dbdc118c6ea619796f48021208a18795a55f59c
Nick Klingensmith <programmerpichu@gmail.com>
2020-03-30 16:26:14 -0700

    -Added Vec2 Angle and AngleBetween methods and tests!

--- Examples/StereoKitTest/Docs/DocVec.cs
+++ Examples/StereoKitTest/Docs/DocVec.cs
@@ -58,12 +58,57 @@ class DocVec : ITest
         return visible && dotPerpendicular && dotInvisible;
     }
 
+    static bool TestAngles()
+    {
+        /// :CodeSample: Vec2.Angle
+        Vec2 point = new Vec2(1, 0);
+        float angle0 = point.Angle();
+
+        point = new Vec2(0, 1);
+        float angle90 = point.Angle();
+
+        point = new Vec2(-1, 0);
+        float angle180 = point.Angle();
+
+        point = new Vec2(0, -1);
+        float angle270 = point.Angle();
+        /// :End:
+        
+        point = new Vec2(2.75f, 2.75f);
+        float angle45 = point.Angle();
+
+        Log.Info($"Angles 0 == {angle0}, Angles 45 == {angle45}, 90 == {angle90}, 180 == {angle180}, 270 == {angle270}");
+        return angle0 == 0 && angle90 == 90 && angle180 == 180 && angle270 == 270;
+    }
+
+    static bool TestAngleBetween()
+    {
+        /// :CodeSample: Vec2.AngleBetween
+        Vec2 directionA = new Vec2( 1, 1);
+        Vec2 directionB = new Vec2(-1, 1);
+        float angle90 = Vec2.AngleBetween(directionA, directionB);
+
+        directionA = new Vec2(1, 1);
+        directionB = new Vec2(0,-2);
+        float angleNegative135 = Vec2.AngleBetween(directionA, directionB);
+        /// :End:
+
+        directionA = new Vec2(1, 1);
+        directionB = new Vec2(0, 1);
+        float angle45 = Vec2.AngleBetween(directionA, directionB);
+
+        Log.Info($"AngleBetween 45 == {angle45}, 90 == {angle90}, -135 == {angleNegative135}");
+        return angle45 == 45 && angle90 == 90 && angleNegative135 == -135;
+    }
+
     public void Initialize()
     {
         Tests.Test(TestDistance);
         Tests.Test(TestDistanceSq);
         Tests.Test(TestLerp);
         Tests.Test(TestDot);
+        Tests.Test(TestAngles);
+        Tests.Test(TestAngleBetween);
     }
 
     public void Shutdown(){}
--- StereoKit/Math/Vec2.cs
+++ StereoKit/Math/Vec2.cs
@@ -1,22 +1,26 @@
 ﻿
+using System;
 using System.Runtime.InteropServices;
 
 namespace StereoKit
 {
-    /// <summary>A vector with 2 components: x and y. This can represent a point in 2D space,
-    /// a directional vector, or any other sort of value with 2 dimensions to it!</summary>
+    /// <summary>A vector with 2 components: x and y. This can represent a 
+    /// point in 2D space, a directional vector, or any other sort of value 
+    /// with 2 dimensions to it!</summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct Vec2
     {
         /// <summary>Vector components.</summary>
         public float x, y;
 
-        /// <summary>Magnitude is the length of the vector! Or the distance from the origin
-        /// to this point. Uses Math.Sqrt, so it's not dirt cheap or anything.</summary>
+        /// <summary>Magnitude is the length of the vector! Or the distance 
+        /// from the origin to this point. Uses Math.Sqrt, so it's not dirt 
+        /// cheap or anything.</summary>
         public float Magnitude => SKMath.Sqrt(x*x + y*y);
         
-        /// <summary>This is the squared magnitude of the vector! It skips the Sqrt call, and
-        /// just gives you the squared version for speedy calculations that can work with it squared.</summary>
+        /// <summary>This is the squared magnitude of the vector! It skips 
+        /// the Sqrt call, and just gives you the squared version for speedy 
+        /// calculations that can work with it squared.</summary>
         public float MagnitudeSq => x*x + y*y;
 
         /// <summary>A basic constructor, just copies the values in!</summary>
@@ -28,8 +32,9 @@ namespace StereoKit
             this.y = y;
         }
 
-        /// <summary>Creates a normalized vector (vector with a length of 1) from the
-        /// current vector. Will not work properly if the vector has a length of zero.</summary>
+        /// <summary>Creates a normalized vector (vector with a length of 1) 
+        /// from the current vector. Will not work properly if the vector has 
+        /// a length of zero.</summary>
         /// <returns>The normalized (length of 1) vector!</returns>
         public Vec2 Normalized()
         {
@@ -37,8 +42,9 @@ namespace StereoKit
             return new Vec2(x/mag, y/mag);
         }
 
-        /// <summary>Turns this vector into a normalized vector (vector with a length of 1) from the
-        /// current vector. Will not work properly if the vector has a length of zero.</summary>
+        /// <summary>Turns this vector into a normalized vector (vector with 
+        /// a length of 1) from the current vector. Will not work properly if
+        /// the vector has a length of zero.</summary>
         /// <returns>The normalized (length of 1) vector!</returns>
         public void Normalize()
         {
@@ -46,6 +52,32 @@ namespace StereoKit
             x /= mag;
             y /= mag;
         }
+
+        /// <summary>Returns the counter-clockwise degrees from [1,0]. 
+        /// Resulting value is between 0 and 360. Vector does not need to be
+        /// normalized.</summary>
+        /// <returns>Counter-clockwise angle of the vector in degrees from
+        /// [1,0], as a value between 0 and 360.</returns>
+        public float Angle()
+        {
+            float result = (float)Math.Atan2(y, x) * Units.rad2deg;
+            if (result < 0) result = 360 + result;
+            return result;
+        }
+
+        /// <summary>Calculates a signed angle between two vectors! Sign will
+        /// be positive if B is counter-clockwise (left) of A, and negative 
+        /// if B is clockwise (right) of A. Vectors do not need to be 
+        /// normalized.</summary>
+        /// <param name="a">The first, initial vector, A. Does not need to be 
+        /// normalized.</param>
+        /// <param name="b">The second vector, B, that we're finding the 
+        /// angle to. Does not need to be normalized.</param>
+        /// <returns>a signed angle between two vectors! Sign will be 
+        /// positive if B is counter-clockwise (left) of A, and negative if B 
+        /// is clockwise (right) of A.</returns>
+        public static float AngleBetween(Vec2 a, Vec2 b)
+            => (float)Math.Atan2(a.x*b.y - a.y*b.x, a.x*b.x + a.y*b.y) * Units.rad2deg;
 
         public override string ToString()
         {
