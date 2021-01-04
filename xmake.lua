521b1eae3fd6bf7b321d47c9577522b4e1aab223
Nick Klingensmith <programmerpichu@gmail.com>
2021-01-04 14:27:25 -0800

    -xmake updates

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.release", "mode.debug")
 
 --------------------------------------------------
-
+--[[
 package("reactphysics3d")
     set_homepage("http://www.reactphysics3d.com/")
     set_description("Open source C++ physics engine library in 3D")
@@ -12,7 +12,7 @@ package("reactphysics3d")
         import("package.tools.cmake").install(package, {"-DCMAKE_POSITION_INDEPENDENT_CODE=ON"})
     end)
 package_end()
-
+]]--
 --------------------------------------------------
 
 package("openxr")
@@ -22,16 +22,28 @@ package("openxr")
     add_urls("https://github.com/KhronosGroup/OpenXR-SDK/archive/release-$(version).tar.gz")
     
     on_install("linux", "windows", "android", function (package)
-        import("package.tools.cmake").install(package, {"-DCMAKE_POSITION_INDEPENDENT_CODE=ON"})
+        import("package.tools.cmake").install(package) -- , {"-DCMAKE_POSITION_INDEPENDENT_CODE=ON"})
     end)
 package_end()
 
 --------------------------------------------------
 
-add_requires("reactphysics3d 0.8.0", {verify = false, configs = {vs_runtime="MD"}})
-add_requires("openxr 1.0.13",        {verify = false, configs = {vs_runtime="MD"}})
+--add_requires("reactphysics3d 0.8.0", {verify = false, configs = {vs_runtime="MD"}})
 
+-- On Android, we have a precompiled binary provided by Oculus
+if not is_plat("android") then
+    add_requires("openxr 1.0.13", {verify = false, configs = {vs_runtime="MD"}})
+end
+
+option("uwp")
+    set_default(false)
+    set_showmenu("true")
+    set_description("Build for UWP")
+    set_values(true, false)
+    add_defines("WINDOWS_UWP", "WINAPI_FAMILY=WINAPI_FAMILY_APP")
+    
 target("StereoKitC")
+    add_options("uwp")
     set_version("0.3.0")
     set_kind("shared")
     if (is_plat("windows")) then
@@ -50,18 +62,27 @@ target("StereoKitC")
     add_includedirs("StereoKitC/lib/include")
     add_includedirs("StereoKitC/lib/include_no_win")
 
-    add_packages("reactphysics3d", "openxr")
-    if is_plat("linux", "android") then 
-        add_links("OpenGL", "GLEW", "GLX", "X11")
+    if has_config("uwp") then
+        add_defines("_WINRT_DLL", "_WINDLL", "__WRL_NO_DEFAULT_LIB__")
+    end
+
+    --add_packages("reactphysics3d")
+    if is_plat("android") then
+        add_linkdirs("StereoKitC/lib/bin/$(arch)/$(mode)")
+        add_links("openxr_loader")
+    else
+        add_packages("openxr")
     end
 
     on_load(function (target)
-        if is_plat("linux", "android") then
+        if is_plat("linux") then
             target:add(find_packages("GL", "GLEW", "glx", "X11"))
+        elseif is_plat("android") then
+            target:add(find_packages("EGL", "OpenSLES"))
         end
     end)
 
-    -- This doesn't actually work yet either
+    -- This doesn't actually work yet
     if is_plat("wasm") then
         add_cxflags("-msimd128", "-msse4", "-s", "SIMD=1")
     end
@@ -75,8 +96,21 @@ target("StereoKitC")
 
 if is_plat("linux", "windows") then
     target("StereoKitCTest")
+        add_options("uwp")
         set_kind("binary")
         add_files("Examples/StereoKitCTest/*.cpp")
         add_includedirs("StereoKitC/")
         add_deps("StereoKitC")
 end
+
+--------------------------------------------------
+--[[
+if is_plat("android") then
+    target("StereoKitCTest_Android")
+        set_kind("binary")
+        add_files("Examples/StereoKitCTest_Android/StereoKitCTest_Android.NativeActivity/*.cpp")
+        add_files("Examples/StereoKitCTest_Android/StereoKitCTest_Android.NativeActivity/*.c")
+        add_includedirs("StereoKitC/")
+        add_deps("StereoKitC")
+end
+]]--
