b8b50d5db97777696d31348f8daefb46e5ed2f46
Nick Klingensmith <programmerpichu@gmail.com>
2021-04-28 15:36:15 -0700

    -Adding some commentdocs, and building some demos!

--- Examples/StereoKitTest/Demos/DemoMicrophone.cs
+++ Examples/StereoKitTest/Demos/DemoMicrophone.cs
@@ -8,9 +8,7 @@ class DemoMicrophone : ITest
 	Matrix titlePose   = Matrix.TRS(V.XYZ(-0.5f, 0.05f, -0.5f), Quat.LookDir(1, 0, 1), 2);
 	string title       = "Microphone";
 
-	Pose     windowPose = new Pose(0.5f,0,-0.5f, Quat.LookDir(-1,0,1));
-	string[] devices;
-	string   activeDevice;
+	
 	Sprite   micSprite;
 	Material micMaterial;
 	float[]  micBuffer = new float[400];
@@ -19,12 +17,14 @@ class DemoMicrophone : ITest
 
 	public void Initialize()
 	{
-		micSprite = Sprite.FromFile("mic_icon.png", SpriteType.Single);
+		micSprite   = Sprite.FromFile("mic_icon.png", SpriteType.Single);
 		micMaterial = Default.MaterialUnlit.Copy();
 		micMaterial.Transparency = Transparency.Blend;
 
-		devices = Microphone.GetDevices();
+		string[] micDevices = Microphone.GetDevices();
 		Microphone.Start();
+
+		Tests.RunForFrames(2);
 	}
 
 	public void Shutdown()
@@ -32,20 +32,58 @@ class DemoMicrophone : ITest
 		Microphone.Stop();
 	}
 
-	void SetMic(string name)
+	/// :CodeSample: Microphone Microphone.GetDevices Microphone.Start
+	/// ### Choosing a microphone device
+	/// While generally you'll prefer to use the default device, it can be
+	/// nice to allow users to pick which mic they're using! This is
+	/// especially important on PC, where users may have complicated or
+	/// interesting setups.
+	/// 
+	/// ![Microphone device selection window]({{site.screen_url}}/MicrophoneSelector.jpg)
+	/// 
+	/// This sample is a very simple window that allows users to start
+	/// recording with a device other than the default. NOTE: this example
+	/// is designed with the assumption that Microphone.Start() has been
+	/// called already.
+	Pose     micSelectPose   = new Pose(0.5f, 0, -0.5f, Quat.LookDir(-1, 0, 1));
+	string[] micDevices      = null;
+	string   micDeviceActive = null;
+	void ShowMicDeviceWindow()
 	{
-		activeDevice = name;
-		Microphone.Start(name);
+		// Let the user choose a microphone device
+		UI.WindowBegin("Available Microphones:", ref micSelectPose);
+
+		// User may plug or unplug a mic device, so it's nice to be able to
+		// refresh this list.
+		if (UI.Button("Refresh") || micDevices == null)
+			micDevices = Microphone.GetDevices();
+		UI.HSeparator();
+
+		// Display the list of potential microphones. Some systems may only
+		// have the default (null) device available.
+		Vec2 size = V.XY(0.25f, UI.LineHeight);
+		if (UI.Radio("Default", micDeviceActive == null, size))
+		{
+			micDeviceActive = null;
+			Microphone.Start(micDeviceActive);
+		}
+		foreach (string device in micDevices)
+		{
+			if (UI.Radio(device, micDeviceActive == device, size))
+			{
+				micDeviceActive = device;
+				Microphone.Start(micDeviceActive);
+			}
+		}
+
+		UI.WindowEnd();
 	}
+	/// :End:
+
 	public void Update()
 	{
-		Vec2 size = V.XY(0.25f, UI.LineHeight);
-
-		UI.WindowBegin("Available Microphones:", ref windowPose);
-		if (UI.Radio("Default", activeDevice == null, size)) SetMic(null);
-		foreach(string device in devices)
-			if (UI.Radio(device, activeDevice == device, size)) SetMic(device);
-		UI.WindowEnd();
+		ShowMicDeviceWindow();
+		Tests.Screenshot(1, 400, 400, "MicrophoneSelector.jpg", micSelectPose.position+V.XYZ(-0.15f, -0.02f, 0.15f), micSelectPose.position-V.XYZ(0,0.12f,0));
 
 		if (Microphone.IsRecording)
 		{
@@ -62,8 +100,6 @@ class DemoMicrophone : ITest
 				micIntensityDest = 1 - (avg * avg);
 			}
 
-
-			Log.Info(micIntensity.ToString());
 			micIntensity = SKMath.Lerp(micIntensity, micIntensityDest, 16 * Time.Elapsedf);
 			float scale = 0.1f + 0.1f * micIntensity;
 			Color color = new Color(1,1,1, Math.Max(0.1f, micIntensity));
--- StereoKit/Systems/Microphone.cs
+++ StereoKit/Systems/Microphone.cs
@@ -3,13 +3,35 @@ using System.Runtime.InteropServices;
 
 namespace StereoKit
 {
+	/// <summary>This class provides access to the hardware's microphone, and
+	/// stores it in a Sound stream. Start and Stop recording, and check the
+	/// Sound property for the results! Remember to ensure your application
+	/// has microphone permissions enabled!</summary>
 	public static class Microphone
 	{
 		private static Sound micSound;
 
+		/// <summary>This is the sound stream of the Microphone when it is
+		/// recording. If the Microphone is not recording, this will be null.
+		/// The sound asset does not exist until the first call to Start, and
+		/// is re-used when starting a recording. It will not be deleted 
+		/// until shutdown, so it's safe to store a reference to this if you
+		/// really want.</summary>
 		public static Sound Sound       { get => micSound; }
+		/// <summary>Tells if the Microphone is currently recording audio.
+		/// </summary>
 		public static bool  IsRecording { get => NativeAPI.mic_is_recording(); }
 
+		/// <summary>Constructs a list of valid Microphone devices attached
+		/// to the system. These names can be passed into Start to select
+		/// a specific device to record from. It's recommended to cache this
+		/// list if you're using it frequently, as this list is constructed 
+		/// each time you call it.
+		/// 
+		/// It's good to note that a user might occasionally plug or unplug
+		/// microphone devices from their system, so this list may
+		/// occasionally change.</summary>
+		/// <returns>List of human readable microphone device names.</returns>
 		public static string[] GetDevices()
 		{
 			int      count  = NativeAPI.mic_device_count();
@@ -19,6 +41,22 @@ namespace StereoKit
 
 			return result;
 		}
+
+		/// <summary>This begins recording audio from the Microphone! Audio
+		/// is stored in Microphone.Sound as a stream of audio. If the
+		/// Microphone is already recording with a different device, it will
+		/// stop the previous recording and start again with the new device.
+		/// 
+		/// If null is provided as the device, then they system's default
+		/// input device will be used. Some systems may not provide access
+		/// to devices other than the system's default.</summary>
+		/// <param name="deviceName">The name of the microphone device to
+		/// use, as seen in the GetDevices list. null will use the system's
+		/// default device preference.</param>
+		/// <returns>True if recording started successfully, false for 
+		/// failure. This could fail if the app does not have mic permissions,
+		/// or if the deviceName is for a mic that has since been unplugged.
+		/// </returns>
 		public static bool Start(string deviceName = null)
 		{
 			IntPtr ptr = NativeAPI.mic_start(deviceName);
@@ -28,6 +66,8 @@ namespace StereoKit
 				micSound = null;
 			return micSound == null;
 		}
+		/// <summary>If the Microphone is recording, this will stop it.
+		/// </summary>
 		public static void Stop()
 		{
 			NativeAPI.mic_stop();
