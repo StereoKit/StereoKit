007076c82614f2e3b04ab5e78732e11e26777c8c
Nick Klingensmith <programmerpichu@gmail.com>
2021-06-03 17:55:37 -0700

    -Support 2-3 argument Actions in the docs.

--- StereoKitDocumenter/StringHelper.cs
+++ StereoKitDocumenter/StringHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace StereoKitDocumenter
 {
@@ -43,6 +44,32 @@ namespace StereoKitDocumenter
 						: type;
 				}
 			}
+		}
+
+		public static List<string> SeparateGroupedString(char separator, string str)
+		{
+			List<string> result = new List<string>();
+			string curr = "";
+
+			int bracketCt = 0;
+			int parenCt = 0;
+			int braceCt = 0;
+			for (int i = 0; i < str.Length; i++)
+			{
+				if      (str[i] == '{') braceCt++;
+				else if (str[i] == '[') bracketCt++;
+				else if (str[i] == '(') parenCt++;
+				else if (str[i] == '}') braceCt--;
+				else if (str[i] == ']') bracketCt--;
+				else if (str[i] == ')') parenCt--;
+				if (braceCt == 0 && parenCt == 0 && bracketCt == 0 && str[i] == separator) { 
+					result.Add(curr);
+					curr = "";
+				}
+				else curr += str[i];
+			}
+			result.Add( curr );
+			return result;
 		}
 	}
 }
