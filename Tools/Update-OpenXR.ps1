ad8ad3fe995564c84eaabda887cbd7c65d8b4f13
Nick Klingensmith <programmerpichu@gmail.com>
2021-06-08 18:35:46 -0700

    -Updates to OpenXR build script for Windows, allows binaries to be kept out of the repository

--- Tools/Update-OpenXR.ps1
+++ Tools/Update-OpenXR.ps1
@@ -1,3 +1,20 @@
+Push-Location -Path $PSScriptRoot
+
+# Check the version installed
+$openxrDesired = Select-String -Path "..\xmake.lua" -Pattern 'add_requires\("openxr_loader (.*?)"' | %{$_.Matches.Groups[1].Value}
+if (Test-Path -Path oxr_current.txt -PathType Leaf) {
+    $openxrCurrent = Get-Content -Path "oxr_current.txt"
+} else {
+    $openxrCurrent = ""
+}
+
+if ($openxrCurrent -ne $openxrDesired) {
+    Write-Host "Updating to the correct OpenXR loader version!"
+    Write-Host "$openxrCurrent -> $openxrDesired"
+} else {
+    Write-Host "OpenXR is up-to-date"
+    exit
+}
 
 # Get the Visual Studio executable for building
 $vsExe = & "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -property productPath
@@ -30,15 +47,16 @@ function Build-Config {
     Write-Host "Built $config complete!" -ForegroundColor green
 
     #copy files into the project
-    Copy-Item -Path "src\loader\Release\openxr_loader.lib" -Destination "..\..\..\StereoKitC\lib\bin\$config\Release\openxr_loader.lib" -Force -Confirm:$false
-    Copy-Item -Path "src\loader\Debug\openxr_loaderd.lib" -Destination "..\..\..\StereoKitC\lib\bin\$config\Debug\openxr_loader.lib" -Force -Confirm:$false
-    Copy-Item -Path "src\loader\openxr_loader.dir\Debug\openxr_loader.pdb" -Destination "..\..\..\StereoKitC\lib\bin\$config\Debug\openxr_loader.pdb" -Force -Confirm:$false
+    Copy-Item -Path "src\loader\Release\openxr_loader.lib" -Destination "..\..\..\..\StereoKitC\lib\bin\$config\Release\openxr_loader.lib" -Force -Confirm:$false
+    Copy-Item -Path "src\loader\Debug\openxr_loaderd.lib" -Destination "..\..\..\..\StereoKitC\lib\bin\$config\Debug\openxr_loader.lib" -Force -Confirm:$false
+    Copy-Item -Path "src\loader\openxr_loader.dir\Debug\openxr_loader.pdb" -Destination "..\..\..\..\StereoKitC\lib\bin\$config\Debug\openxr_loader.pdb" -Force -Confirm:$false
 
     Pop-Location
 }
 
 # Clone the repository
 & git clone "https://github.com/KhronosGroup/OpenXR-SDK.git"
+& git checkout "release-$openxrDesired"
 Push-Location -Path "OpenXR-SDK"
 
 # Create build folders
@@ -84,9 +102,14 @@ Pop-Location
 
 # Copy include files
 Write-Host 'Copying headers'
-Copy-Item -Path "include\openxr\*.h" -Destination "..\StereoKitC\lib\include\openxr" -Force -Confirm:$false
+Copy-Item -Path "include\openxr\*.h" -Destination "..\..\StereoKitC\lib\include\openxr" -Force -Confirm:$false
 Write-Host 'Headers copied!' -ForegroundColor green
 
 # Clean up and delete files!
 Pop-Location
 Remove-Item -Path "OpenXR-SDK" -Recurse -Force -Confirm:$false
+
+# Mark the current OpenXR version down
+Set-Content -Path 'oxr_current.txt' -Value $openxrDesired
+
+Pop-Location
