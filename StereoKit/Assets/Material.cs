83a23781bbd7f3f4e30f5e102482cc2ea52cf005
Faolan <46481567+Faolan-Rad@users.noreply.github.com>
2021-12-14 09:01:24 -0500

    Fixed Format problems and name problems of Params

--- StereoKit/Assets/Material.cs
+++ StereoKit/Assets/Material.cs
@@ -278,27 +278,53 @@ namespace StereoKit
 		public void SetTexture(string name, Tex value)
 			=> NativeAPI.material_set_texture(_inst, name, value._inst);
 
-		/// <summary>Gets all shader parameters Name and Type</summary>
-		public IEnumerable<(string Name,MaterialParam Type)> GetAllParametersInfo()
+		/// <summary>
+		/// The name and type of a Parameter on a Material
+		/// </summary>
+		public struct ParamInfo
+		{
+			/// <summary>
+			/// The name of the param
+			/// </summary>
+			public string Name;
+
+			/// <summary>
+			/// The type of the param 
+			/// </summary>
+			public MaterialParam Type;
+			/// <summary>
+			/// Parameter Info constructor 
+			/// </summary>
+			/// <param name="name">Name of the shader parameter.</param>
+			/// <param name="type">New type for the parameter.</param>
+			internal ParamInfo(string name, MaterialParam type)
+			{
+				Name = name;
+				Type = type;
+			}
+		}
+
+        /// <summary>Gets all shader parameters Name and Type</summary>
+        public IEnumerable<ParamInfo> GetAllParamsInfo()
         {
 			var count = NativeAPI.material_get_param_count(_inst);
             for (int i = 0; i < count; i++)
             {
-				NativeAPI.material_get_param_info(_inst, i, out var name, out var type);
-				yield return (name, type);
+				NativeAPI.material_get_param_info(_inst, i, out string name, out MaterialParam type);
+				yield return new ParamInfo(name, type);
 			}
 		}
-		/// <summary>Gets shader parameter Name and Type</summary>
-		/// <param name="index">Index of the shader parameter.</param>
-		public (string Name, MaterialParam Type) GetParameterInfo(int index)
+        /// <summary>Gets shader parameter Name and Type</summary>
+        /// <param name="index">Index of the shader parameter.</param>
+        public ParamInfo GetParamInfo(int index)
 		{
 			var count = NativeAPI.material_get_param_count(_inst);
-			if(count <= index)
-            {
+			if((count <= index) || index < 0)
+			{
 				throw new IndexOutOfRangeException("Param not found");
-            }
-			NativeAPI.material_get_param_info(_inst, index, out var name, out var type);
-			return (name, type);
+			}
+			NativeAPI.material_get_param_info(_inst, index, out string name, out MaterialParam type);
+			return new ParamInfo(name, type);
 		}
 
 
