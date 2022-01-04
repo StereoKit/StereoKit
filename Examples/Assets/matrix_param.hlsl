b373f84b60a9d0e49ba0d8fd6cbf5a4049dbad6a
Nick Klingensmith <programmerpichu@gmail.com>
2022-01-03 16:50:47 -0800

    - Added a test for matrix parameters to shaders.

---
+++ Examples/Assets/matrix_param.hlsl
@@ -0,0 +1,48 @@
+#include <stereokit.hlsli>
+
+//--name = app/matrix_param
+// This shader is a test for using matrices as parameters. Instead of using the
+// instance transform that StereoKit provides, it uses a transform provided
+// from the user.
+
+//--custom_transform = 1,0,0,0,  0,1,0,0,  0,0,1,0,  0,0,0,1
+float4x4 custom_transform;
+//--color:color = 1, 1, 1, 1
+float4 color;
+
+//--diffuse     = white
+Texture2D    diffuse   : register(t0);
+SamplerState diffuse_s : register(s0);
+
+struct vsIn {
+	float4 pos  : SV_Position;
+	float3 norm : NORMAL0;
+	float2 uv   : TEXCOORD0;
+	float4 col  : COLOR0;
+};
+struct psIn {
+	float4 pos   : SV_POSITION;
+	float2 uv    : TEXCOORD0;
+	float4 color : COLOR0;
+	uint view_id : SV_RenderTargetArrayIndex;
+};
+
+psIn vs(vsIn input, uint id : SV_InstanceID) {
+	psIn o;
+	o.view_id = id % sk_view_count;
+	id        = id / sk_view_count;
+
+	float3 world = mul(float4(input.pos.xyz, 1), custom_transform).xyz;
+	o.pos        = mul(float4(world,         1), sk_viewproj[o.view_id]);
+
+	o.uv    = input.uv;
+	o.color = input.col * color * sk_inst[id].color;
+	return o;
+}
+float4 ps(psIn input) : SV_TARGET {
+	float4 col = diffuse.Sample(diffuse_s, input.uv);
+
+	col = col * input.color;
+
+	return col; 
+}
