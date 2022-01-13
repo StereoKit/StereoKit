c7022ad7fc580ced404f979f8e0aece6d0d723df
Nick Klingensmith <programmerpichu@gmail.com>
2022-01-12 22:35:22 -0800

    - v0.3.5-preview.6

--- StereoKitC/stereokit.h
+++ StereoKitC/stereokit.h
@@ -5,7 +5,7 @@
 #define SK_VERSION_MAJOR 0
 #define SK_VERSION_MINOR 3
 #define SK_VERSION_PATCH 5
-#define SK_VERSION_PRERELEASE 5
+#define SK_VERSION_PRERELEASE 6
 
 #if defined(__GNUC__) || defined(__clang__)
 	#define SK_DEPRECATED __attribute__((deprecated))
