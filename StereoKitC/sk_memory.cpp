323363e04d129d5dc34b509879ebd5e9b3696766
Nick Klingensmith <programmerpichu@gmail.com>
2022-02-25 23:01:19 -0800

    - Added a memory alloc function that also zeros out the data

--- StereoKitC/sk_memory.cpp
+++ StereoKitC/sk_memory.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <string.h>
 
 namespace sk {
 
@@ -9,9 +10,21 @@ namespace sk {
 void *sk_malloc(size_t bytes) {
 	void *result = malloc(bytes);
 	if (result == nullptr && bytes > 0) {
-		fprintf(stderr, "Memory allocation failed!");
+		fprintf(stderr, "Memory alloc failed!");
 		abort();
 	}
+	return result;
+}
+
+///////////////////////////////////////////
+
+void *sk_malloc_zero(size_t bytes) {
+	void *result = malloc(bytes);
+	if (result == nullptr && bytes > 0) {
+		fprintf(stderr, "Memory alloc failed!");
+		abort();
+	}
+	memset(result, 0, bytes);
 	return result;
 }
 
@@ -20,7 +33,7 @@ void *sk_malloc(size_t bytes) {
 void *sk_realloc(void *memory, size_t bytes) {
 	void *result = realloc(memory, bytes);
 	if (result == nullptr && bytes > 0) {
-		fprintf(stderr, "Memory re-allocation failed!");
+		fprintf(stderr, "Memory alloc failed!");
 		abort();
 	}
 	return result;
