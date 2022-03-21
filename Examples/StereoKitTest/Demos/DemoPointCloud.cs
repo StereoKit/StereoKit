b69363975b36d519218aab6ef3deb12fde829fd0
Nick Klingensmith <programmerpichu@gmail.com>
2022-03-21 16:38:42 -0700

    - Improvements to the point cloud tool and demo. Works with a Model now too :)

--- Examples/StereoKitTest/Demos/DemoPointCloud.cs
+++ Examples/StereoKitTest/Demos/DemoPointCloud.cs
@@ -1,9 +1,6 @@
 ﻿using StereoKit;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StereoKitTest.Demos
 {
@@ -16,8 +13,8 @@ namespace StereoKitTest.Demos
 		bool  distanceIndependant;
 		float pointSize;
 
-		public bool  DistanceIndependantSize { get => distanceIndependant; set { mat["screen_size"] = value?1:0; distanceIndependant = value; } }
-		public float PointSize               { get => pointSize;           set { mat["point_size" ] = value;     pointSize           = value; } }
+		public bool  DistanceIndependantSize { get => distanceIndependant; set { mat["screen_size"] = value?1.0f:0.0f; distanceIndependant = value; } }
+		public float PointSize               { get => pointSize;           set { mat["point_size" ] = value;           pointSize           = value; } }
 
 		public PointCloud(float pointSizeMeters = 0.01f)
 		{
@@ -28,6 +25,8 @@ namespace StereoKitTest.Demos
 			: this (pointSizeMeters) => SetPoints(fromMesh);
 		public PointCloud(float pointSizeMeters, Vertex[] fromVerts)
 			: this(pointSizeMeters) => SetPoints(fromVerts);
+		public PointCloud(float pointSizeMeters, Model fromModel)
+			: this(pointSizeMeters) => SetPoints(fromModel);
 
 		public void Draw(Matrix transform) => mesh?.Draw(mat, transform);
 
@@ -45,10 +44,10 @@ namespace StereoKitTest.Demos
 			{
 				Vertex p = points[i];
 				int idx = i*4;
-				verts[idx  ] = new Vertex(p.pos, Vec3.Up, V.XY(-.5f, .5f), p.col);
-				verts[idx+1] = new Vertex(p.pos, Vec3.Up, V.XY( .5f, .5f), p.col);
-				verts[idx+2] = new Vertex(p.pos, Vec3.Up, V.XY( .5f,-.5f), p.col);
-				verts[idx+3] = new Vertex(p.pos, Vec3.Up, V.XY(-.5f,-.5f), p.col);
+				verts[idx  ] = new Vertex(p.pos, p.norm, V.XY(-.5f, .5f), p.col);
+				verts[idx+1] = new Vertex(p.pos, p.norm, V.XY( .5f, .5f), p.col);
+				verts[idx+2] = new Vertex(p.pos, p.norm, V.XY( .5f,-.5f), p.col);
+				verts[idx+3] = new Vertex(p.pos, p.norm, V.XY(-.5f,-.5f), p.col);
 			}
 			if (mesh == null) {
 				uint[] inds = new uint[points.Length*6];
@@ -71,15 +70,62 @@ namespace StereoKitTest.Demos
 		}
 		public void SetPoints(Mesh fromMeshVerts)
 			=> SetPoints(fromMeshVerts.GetVerts());
+		public void SetPoints(Model fromModelVerts)
+			=> SetPoints(fromModelVerts.Visuals
+				.SelectMany(v => TransformVerts(v.ModelTransform, v.Mesh.GetVerts()))
+				.ToArray   ());
+
+		private static Vertex[] TransformVerts(Matrix mat, Vertex[] verts)
+		{
+			for (int i = 0; i < verts.Length; i++)
+				verts[i].pos = mat.Transform(verts[i].pos);
+			return verts;
+		}
 	}
 
 	class DemoPointCloud : ITest
 	{
+		Matrix     descPose     = Matrix.TR (-0.5f, 0, -0.5f, Quat.LookDir(1,0,1));
+		string     description  = "Point clouds are not a built-in feature of StereoKit, but it's not hard to do this yourself! Check out the code for this demo for a class that'll help you do this directly from data, or from a Model.";
+		Matrix     titlePose    = Matrix.TRS(V.XYZ(-0.5f, 0.05f, -0.5f), Quat.LookDir(1, 0, 1), 2);
+		string     title        = "Point Clouds";
+
+		Pose       cloudPose    = new Pose(0.5f, 0, -0.5f, Quat.LookDir(-1, 0, 1));
+		float      cloudScale   = 1;
 		PointCloud cloud;
+
+		Pose       settingsPose = new Pose(0.8f, 0.05f, -0.2f, Quat.LookDir(-1, 0, 1));
+		float      pointSize    = 0.01f;
+
 		public void Initialize()
 		{
-			Model model = Model.FromFile("DamagedHelmet.gltf");
-			cloud = new PointCloud(0.01f, model.Visuals[0].Mesh);
+			// Generate colored points in the shape of a UV sphere! This tool
+			// re-uses the Vertex type, but doesn't particularly need the
+			// normals, and will replace the UVs. You could pretty easily add
+			// lighting to the points using the normals if you wanted to :)
+			const int xCount = 24;
+			const int yCount = 16;
+			Vertex[] points = new Vertex[xCount*yCount];
+			for (int y = 0; y < yCount; y++)
+			{
+				for (int x = 0; x < xCount; x++)
+				{
+					float u = x / (float)xCount;
+					float v = y / (float)yCount;
+					int   i = x + y * xCount;
+
+					float theta = u * (float)Math.PI * 2.0f;
+					float phi   = (v - 0.5f) * (float)Math.PI;
+					float c     = SKMath.Cos(phi);
+
+					points[i].pos = V.XYZ(c * SKMath.Cos(theta), SKMath.Sin(phi), c * SKMath.Sin(theta))*0.2f;
+					points[i].col = Color.HSV(u, v, 1).ToLinear();
+				}
+			}
+
+			// Make a point cloud out of the points!
+			cloud      = new PointCloud(pointSize, points);
+			cloudScale = 1;
 		}
 
 		public void Shutdown()
@@ -88,12 +134,39 @@ namespace StereoKitTest.Demos
 
 		public void Update()
 		{
-			cloud.Draw(Matrix.Identity);
+			cloud.Draw(cloudPose.ToMatrix(cloudScale));
 
-			if (Input.Key(Key.O).IsJustActive()) cloud.DistanceIndependantSize = true;
-			if (Input.Key(Key.P).IsJustActive()) cloud.DistanceIndependantSize = false;
-			if (Input.Key(Key.BracketOpen ).IsJustActive()) cloud.PointSize -= 0.0025f;
-			if (Input.Key(Key.BracketClose).IsJustActive()) cloud.PointSize += 0.0025f;
+			UI.WindowBegin("Point Cloud", ref settingsPose);
+			{
+				if (UI.Button("Load Model"))
+				{
+					Platform.FilePicker(PickerMode.Open, (file) => {
+						Model model = Model.FromFile(file);
+						cloud      = new PointCloud(pointSize, model);
+						cloudScale = 0.5f / model.Bounds.dimensions.Length;
+					}, null, "*.gltf", "*.glb");
+				}
+				UI.HSlider("Cloud Scale", ref cloudScale, 0.001f, 2, 0);
+
+				UI.PanelBegin();
+				UI.Label("Point Cloud Settings");
+				UI.HSeparator();
+				UI.Label("Mode:", V.XY(.04f, 0));
+				UI.SameLine();
+				if (UI.Radio("Fixed", cloud.DistanceIndependantSize)) cloud.DistanceIndependantSize = true;
+				UI.SameLine();
+				if (UI.Radio("Perspective", !cloud.DistanceIndependantSize)) cloud.DistanceIndependantSize = false;
+
+				UI.Label("Size:", V.XY(.04f, 0));
+				UI.SameLine();
+				if (UI.HSlider("Point Size", ref pointSize, 0.001f, 0.1f, 0))
+					cloud.PointSize = pointSize;
+				UI.PanelEnd();
+			}
+			UI.WindowEnd();
+
+			Text.Add(title, titlePose);
+			Text.Add(description, descPose, V.XY(0.4f, 0), TextFit.Wrap, TextAlign.TopCenter, TextAlign.TopLeft);
 		}
 	}
 }
