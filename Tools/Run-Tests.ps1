daac1a548cb721deda2511209b6133cc89bb373b
Nick Klingensmith <programmerpichu@gmail.com>
2022-03-27 15:48:17 -0700

    - Improvements to Windows + Linux test script

--- Tools/Run-Tests.ps1
+++ Tools/Run-Tests.ps1
@@ -3,13 +3,13 @@ $linux_folder = $linux_folder.replace('\', '/')
 $linux_folder = $linux_folder.replace(':', '')
 $linux_folder = '/mnt/'+$linux_folder
 $linux_folder = $linux_folder.replace('/C/', '/c/')
-Write-Output $linux_folder
 
 cmd /c "wsl cd '${linux_folder}/../' ; xmake f -p linux -a x64 -m release -y ; xmake ;"
-cmd /c "wsl cd '${linux_folder}/../Examples/StereoKitTest/' ; dotnet build -p:OutputPath=bin StereoKitTest.csproj ; cd bin ; LIBGL_ALWAYS_SOFTWARE=1 dotnet run --project ../StereoKitTest.csproj -test -screenfolder ../../../Tools/Screenshots/Linux"
+cmd /c "wsl cd '${linux_folder}/../Examples/StereoKitTest/' ; dotnet restore StereoKitTest.csproj ; dotnet clean -p:SKTest=True StereoKitTest.csproj ; dotnet build -p:SKTest=True StereoKitTest.csproj ; cd bin ; LIBGL_ALWAYS_SOFTWARE=1 dotnet run -p:SKTest=True --project ../StereoKitTest.csproj -test -screenfolder '${linux_folder}/Screenshots/Linux'"
 
 & 'cd' '../Examples/StereoKitTest/'
-& 'dotnet' 'build' '-p:OutputPath=bin' 'StereoKitTest.csproj'
-& 'cd' 'bin'
-& 'dotnet' 'run' '-p:OutputPath=bin' '--project' '../StereoKitTest.csproj' '-test' '-screenfolder' '../../../Tools/Screenshots/Windows'
-& 'cd' '../../../Tools/'
+& 'dotnet' 'restore' 'StereoKitTest.csproj'
+& 'dotnet' 'clean' '-p:SKTest=True' 'StereoKitTest.csproj'
+& 'dotnet' 'build' '-p:SKTest=True' 'StereoKitTest.csproj'
+& 'dotnet' 'run' '-p:SKTest=True' '--project' 'StereoKitTest.csproj' '-test' '-screenfolder' "${PSScriptRoot}\Screenshots\Windows"
+& 'cd' '../../Tools/'
