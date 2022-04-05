9a93748db0fda008cda32646bb8617727dc062f8
Nick Klingensmith <programmerpichu@gmail.com>
2022-04-04 18:46:19 -0700

    - Better description of IU.Input's type parameter.

--- StereoKit/Systems/UI.cs
+++ StereoKit/Systems/UI.cs
@@ -428,9 +428,9 @@ namespace StereoKit
 		/// content in.</param>
 		/// <param name="size">Size of the Input in Hierarchy local meters.
 		/// Zero axes will auto-size.</param>
-		/// <param name="type">
-		/// Specify what type of text field this is if string or number field. This does not limit what can be typed it is just so it knows what keyboard to use
-		/// </param>
+		/// <param name="type">What category of text this Input represents.
+		/// This may affect what kind of soft keyboard will be displayed, if
+		/// one is shown to the user.</param>
 		/// <returns>Returns true every time the contents of 'value' change.
 		/// </returns>
 		public static bool Input(string id, ref string value, Vec2 size = new Vec2(), TextContext type = TextContext.Text) {
