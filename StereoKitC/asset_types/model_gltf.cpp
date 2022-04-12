2f4ebe34670679d72193d7f45307d73b5fe4734f
Nick Klingensmith <programmerpichu@gmail.com>
2022-04-12 14:56:40 -0700

    - Moved cgltf 'implementation' to its own file.

--- StereoKitC/asset_types/model_gltf.cpp
+++ StereoKitC/asset_types/model_gltf.cpp
@@ -9,21 +9,9 @@
 #include "../libraries/ferr_hash.h"
 #include "../libraries/stref.h"
 #include "../systems/platform/platform_utils.h"
-
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning( disable : 26451 )
-#else
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunknown-pragmas"
-#endif
-#define CGLTF_IMPLEMENTATION
 #include "../libraries/cgltf.h"
-#ifdef _MSC_VER
-#pragma warning(pop)
-#else
-#pragma clang diagnostic pop
-#endif
+
+#include <stdio.h>
 
 namespace sk {
 
