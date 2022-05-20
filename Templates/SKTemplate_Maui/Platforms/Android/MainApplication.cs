bf34c8c770ae6151bc552e0bc5fbd5e48ea81570
Nick Klingensmith <programmerpichu@gmail.com>
2022-05-20 16:45:14 -0700

    - Initial Maui template, Android is still misbehaving.

---
+++ Templates/SKTemplate_Maui/Platforms/Android/MainApplication.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace SKTemplate_Maui;
+
+[Application]
+public class MainApplication : Android.App.Application
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+}
