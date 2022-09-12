b49c18ef02ea93baf7d837ebcb0aa3e9fee77e5e
Nick Klingensmith <programmerpichu@gmail.com>
2022-09-12 12:13:15 -0700

    Merge from input_selection

--- StereoKitC/libraries/unicode.h
+++ StereoKitC/libraries/unicode.h
@@ -91,7 +91,7 @@ inline char32_t utf8_decode(const char *utf8_str, const char **out_next_char, in
 
 ///////////////////////////////////////////
 
-inline int32_t utf8_encode_chars(char32_t ch) {
+inline int32_t utf8_encode_units(char32_t ch) {
 	if      (ch <= 0x7F)     return 1;
 	else if (ch <= 0x07FF)   return 2;
 	else if (ch <= 0xFFFF)   return 3;
@@ -101,24 +101,29 @@ inline int32_t utf8_encode_chars(char32_t ch) {
 
 ///////////////////////////////////////////
 
+inline int32_t utf16_encode_units(char32_t ch) {
+	if (ch < 0x00010000L) return 1;
+	if (ch < 0x00110000L) return 2;
+	return 0;
+}
+
+///////////////////////////////////////////
+
 inline int32_t utf8_encode(char *out, char32_t utf) {
 	if (utf <= 0x7F) {
 		// Plain ASCII
 		out[0] = (char) utf;
-		out[1] = 0;
 		return 1;
 	} else if (utf <= 0x07FF) {
 		// 2-byte unicode
 		out[0] = (char) (((utf >> 6) & 0x1F) | 0xC0);
 		out[1] = (char) (((utf >> 0) & 0x3F) | 0x80);
-		out[2] = 0;
 		return 2;
 	} else if (utf <= 0xFFFF) {
 		// 3-byte unicode
 		out[0] = (char) (((utf >> 12) & 0x0F) | 0xE0);
 		out[1] = (char) (((utf >>  6) & 0x3F) | 0x80);
 		out[2] = (char) (((utf >>  0) & 0x3F) | 0x80);
-		out[3] = 0;
 		return 3;
 	} else if (utf <= 0x10FFFF) {
 		// 4-byte unicode
@@ -126,7 +131,6 @@ inline int32_t utf8_encode(char *out, char32_t utf) {
 		out[1] = (char) (((utf >> 12) & 0x3F) | 0x80);
 		out[2] = (char) (((utf >>  6) & 0x3F) | 0x80);
 		out[3] = (char) (((utf >>  0) & 0x3F) | 0x80);
-		out[4] = 0;
 		return 4;
 	}
 	return 0;
@@ -135,12 +139,14 @@ inline int32_t utf8_encode(char *out, char32_t utf) {
 ///////////////////////////////////////////
 
 inline int32_t utf8_encode_append(char *buffer, size_t size, char32_t ch) {
-	int32_t count = utf8_encode_chars(ch);
+	int32_t count = utf8_encode_units(ch);
 	size_t  len   = strlen(buffer);
 	if (len + count + 1 >= size) {
 		return 0;
 	} else {
-		return utf8_encode(&buffer[len], ch);
+		count = utf8_encode(&buffer[len], ch);
+		buffer[len + count] = '\0';
+		return count;
 	}
 }
 
@@ -159,6 +165,146 @@ inline size_t strlen(const char16_t *str) {
 	const char16_t* curr = str;
 	while (*curr) curr++;
 	return curr-str;
+}
+
+///////////////////////////////////////////
+
+inline size_t utf_charlen(const char* str_utf8) {
+	if (!str_utf8) return 0;
+	
+	// Single byte UTF8 characters all start with 0b0, so anything smaller than
+	// 0b1 must be single byte!
+	const char single_byte = 0b10000000;
+	// The first byte of multibyte UTF8 characters all begin with 0b11, so
+	// anything larger than 0b10111111 is a multibye character indicator!
+	const char multi_byte = 0b10111111;
+	
+	size_t      result = 0;
+	const char* curr   = str_utf8;
+	while (*curr != '\0') {
+		if (*curr < single_byte || *curr > multi_byte) { result += 1; }
+		curr++;
+	}
+	return result;
+}
+
+///////////////////////////////////////////
+
+inline size_t utf_charlen(const char16_t* str_utf16) {
+	if (!str_utf16) return 0;
+	
+	size_t          result = 0;
+	const char16_t* curr   = str_utf16;
+	while (*curr != '\0') {
+		// Pairs start with a value in the 0xD800–0xDBFF range
+		// Singles have a value in the 0x0000–0xD7FF, 0xE000–0xFFFF ranges
+		if (*curr < 0xDC00 || *curr > 0xDFFF) { result += 1; }
+		curr++;
+	}
+	return result;
+}
+
+///////////////////////////////////////////
+
+inline const char* utf_at_char(const char* str_utf8, int32_t index) {
+	if (!str_utf8) return nullptr;
+
+	// Single byte UTF8 characters all start with 0b0, so anything smaller than
+	// 0b1 must be single byte!
+	const char single_byte = 0b10000000;
+	// The first byte of multibyte UTF8 characters all begin with 0b11, so
+	// anything larger than 0b10111111 is a multibye character indicator!
+	const char multi_byte = 0b10111111;
+
+	size_t      result = 0;
+	const char* curr   = str_utf8;
+	while (*curr != '\0') {
+		if (result == index) return curr;
+		if (*curr < single_byte || *curr > multi_byte) { result += 1; }
+		curr++;
+	}
+	return nullptr;
+}
+
+///////////////////////////////////////////
+
+inline const char16_t* utf_at_char(const char16_t* str_utf16, int32_t index) {
+	if (!str_utf16) return nullptr;
+
+	size_t          result = 0;
+	const char16_t* curr = str_utf16;
+	while (*curr != '\0') {
+		if (result == index) return curr;
+		// Pairs start with a value in the 0xD800–0xDBFF range
+		// Singles have a value in the 0x0000–0xD7FF, 0xE000–0xFFFF ranges
+		if (*curr < 0xDC00 || *curr > 0xDFFF) { result += 1; }
+		curr++;
+	}
+	return nullptr;
+}
+
+///////////////////////////////////////////
+
+inline bool utf_remove_chars(char* at_utf8, int32_t count) {
+	if (!at_utf8) return false;
+
+	size_t remove = 0;
+	char*  curr   = at_utf8;
+	for (size_t i = 0; i < count; i++)
+	{
+		if      (*curr == '\0')                      break;
+		else if ((*curr & 0b10000000) == 0         ) remove += 1;
+		else if ((*curr & 0b11100000) == 0b11000000) remove += 2;
+		else if ((*curr & 0b11110000) == 0b11100000) remove += 3;
+		else if ((*curr & 0b11111000) == 0b11110000) remove += 4;
+		curr = at_utf8 + remove;
+	}
+
+	size_t  len = strlen(at_utf8);
+	int32_t end = len - remove;
+	for (size_t i = 0; i < end; i++) 
+		at_utf8[i] = at_utf8[i + remove];
+	at_utf8[end] = '\0';
+}
+
+///////////////////////////////////////////
+
+inline bool utf_insert_char(const char* buffer_utf8_start, size_t buffer_length, char* at_utf8, char32_t character) {
+	int32_t units = utf8_encode_units(character);
+
+	// Ensure there's enough memory in the string
+	if (buffer_length - (at_utf8 - buffer_utf8_start) < units) return false;
+
+	// Shift the string to make room for the new character
+	size_t len = strlen(at_utf8);
+	for (int32_t i = len + units; i >= units; i--)
+		at_utf8[i] = at_utf8[i - units];
+
+	// And insert the new character
+	utf8_encode(at_utf8, character);
+	return true;
+}
+
+///////////////////////////////////////////
+
+inline bool utf_remove_chars(char16_t* at_utf16, int32_t count) {
+	if (!at_utf16) return false;
+
+	size_t    remove = 0;
+	char16_t* curr   = at_utf16;
+	for (size_t i = 0; i < count; i++)
+	{
+		if      (*curr == '\0')                            break;
+		else if (*at_utf16 < 0xD800 || *at_utf16 > 0xDFFF) remove += 1;
+		else                                               remove += 2;
+		curr = at_utf16 + remove;
+	}
+	
+	size_t  len    = strlen(at_utf16);
+	int32_t end    = len - remove;
+	for (size_t i = 0; i < end; i++) 
+		at_utf16[i] = at_utf16[i + remove];
+	at_utf16[end] = '\0';
 }
 
 ///////////////////////////////////////////
@@ -189,25 +335,15 @@ inline bool utf16_decode_fast_b(const char16_t *utf16_str, const char16_t **out_
 
 ///////////////////////////////////////////
 
-inline int32_t utf16_encode_chars(char32_t ch) {
-	if (ch < 0x00010000L) return 1;
-	if (ch < 0x00110000L) return 2;
-	return 0;
-}
-
-///////////////////////////////////////////
-
 inline int32_t utf16_encode(char16_t *out, char32_t utf) {
 	if (utf < 0x00010000L) {
 		out[0] = (char16_t)utf;
-		out[1] = '\0';
 		return 1;
 	} else if (utf < 0x00110000L) {
 		utf -= 0x00010000L;
 
 		out[0] = ((utf >> 10) & 0x3ff) | 0xd800;
 		out[1] = ((utf >> 0 ) & 0x3ff) | 0xdc00;
-		out[2] = '\0';
 		return 2;
 	}
 	return 0;
@@ -215,13 +351,33 @@ inline int32_t utf16_encode(char16_t *out, char32_t utf) {
 
 ///////////////////////////////////////////
 
+inline bool utf_insert_char(const char16_t* buffer_utf16_start, size_t buffer_length, char16_t* at_utf16, char32_t character) {
+	int32_t units = utf16_encode_units(character);
+
+	// Ensure there's enough memory in the string
+	if (buffer_length - (at_utf16 - buffer_utf16_start) < units) return false;
+
+	// Shift the string to make room for the new character
+	size_t len = strlen(at_utf16);
+	for (int32_t i = len+units; i >= units; i--)
+		at_utf16[i] = at_utf16[i - units];
+
+	// And insert the new character
+	utf16_encode(at_utf16, character);
+	return true;
+}
+
+///////////////////////////////////////////
+
 inline int32_t utf16_encode_append(char16_t *buffer, size_t size, char32_t ch) {
-	int32_t count = utf16_encode_chars(ch);
+	int32_t count = utf16_encode_units(ch);
 	size_t  len   = strlen(buffer);
 	if (len + count + 1 >= size) {
 		return 0;
 	} else {
-		return utf16_encode(&buffer[len], ch);
+		count = utf16_encode(&buffer[len], ch);
+		buffer[len + count] = '\0';
+		return count;
 	}
 }
 
