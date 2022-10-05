328f0054efe47c953542b51e791f7f4439f46dab
Nick Klingensmith <programmerpichu@gmail.com>
2022-10-05 15:19:00 -0700

    Generate screenshots for the whole demo!

--- Examples/StereoKitTest/Demos/Demo.cs
+++ Examples/StereoKitTest/Demos/Demo.cs
@@ -8,7 +8,11 @@ abstract class Demo
 
 	public static void ShowSummary(string title, string description)
 	{
-		if (Tests.IsTesting) return;
+		if (Tests.IsTesting)
+		{
+			Tests.Screenshot($"Demos/{title.Replace(" ", "")}.jpg", 1, 480, 270, 60, contentPose.Transform(V.XYZ(0,0.05f,-0.5f)), contentPose.Transform(V.XYZ(0,-0.1f,0)));
+			return;
+		}
 
 		Text.Add(title,       titlePose);
 		Text.Add(description, descPose, V.XY(0.4f, 0), TextFit.Wrap, TextAlign.TopCenter, TextAlign.TopLeft);
