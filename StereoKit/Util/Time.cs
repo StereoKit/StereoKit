6664a9918db4c43e883f476403a7ebb37f09d650
Nick Klingensmith <programmerpichu@gmail.com>
2022-11-17 10:22:39 -0800

    Clearer docs for Time.

--- StereoKit/Util/Time.cs
+++ StereoKit/Util/Time.cs
@@ -6,53 +6,61 @@ namespace StereoKit
 	/// and frame!</summary>
 	public static class Time
 	{
-		/// <summary> How much time has elapsed since StereoKit was
-		/// initialized? 64 bit time precision. </summary>
+		/// <summary> How many seconds have elapsed since StereoKit was
+		/// initialized? 64 bit time precision, calculated at the start of the
+		/// frame. </summary>
 		public static double Total => NativeAPI.time_total();
-		/// <summary> How much time has elapsed since StereoKit was
-		/// initialized? 32 bit time precision. </summary>
+		/// <summary> How many seconds have elapsed since StereoKit was
+		/// initialized? 32 bit time precision, calculated at the start of the
+		/// frame. </summary>
 		public static float  Totalf => NativeAPI.time_totalf();
-		/// <summary> How much time has elapsed since StereoKit was
-		/// initialized? 64 bit time precision. This version is unaffected by
-		/// the Time.Scale value!</summary>
+		/// <summary> How many seconds have elapsed since StereoKit was
+		/// initialized? 64 bit time precision, calculated at the start of the
+		/// frame. This version is unaffected by the Time.Scale value!
+		/// </summary>
 		public static double TotalUnscaled => NativeAPI.time_total_unscaled();
-		/// <summary> How much time has elapsed since StereoKit was
-		/// initialized? 32 bit time precision. This version is unaffected by
-		/// the Time.Scale value!</summary>
+		/// <summary> How many seconds have elapsed since StereoKit was
+		/// initialized? 32 bit time precision, calculated at the start of the
+		/// frame. This version is unaffected by the Time.Scale value!
+		/// </summary>
 		public static float  TotalUnscaledf => NativeAPI.time_totalf_unscaled();
 
-		/// <summary> (Deprecated, use Time.Step) How much time elapsed during
-		/// the last frame? 64 bit time precision.</summary>
+		/// <summary> (Deprecated, use Time.Step) How many seconds have elapsed
+		/// since the last frame? 64 bit time precision, calculated at
+		/// the start of the frame.</summary>
 		[Obsolete("Use Time.Step")]
 		public static double Elapsed => NativeAPI.time_step();
-		/// <summary> (Deprecated, use Time.Stepf) How much time elapsed during
-		/// the last frame? 32 bit time precision.</summary>
+		/// <summary> (Deprecated, use Time.Stepf) How many seconds have
+		/// elapsed since the last frame? 32 bit time precision, calculated at
+		/// the start of the frame.</summary>
 		[Obsolete("Use Time.Stepf")]
 		public static float  Elapsedf => NativeAPI.time_stepf();
-		/// <summary> (Deprecated, use Time.StepUnscaled) How much time elapsed
-		/// during the last frame? 64 bit time precision. This version is
-		/// unaffected by the Time.Scale value!</summary>
+		/// <summary> (Deprecated, use Time.StepUnscaled) How many seconds have
+		/// elapsed during the last frame? 64 bit time precision, calculated at
+		/// the start of the frame. This version is unaffected by the
+		/// Time.Scale value!</summary>
 		[Obsolete("Use Time.StepUnscaled")]
 		public static double ElapsedUnscaled => NativeAPI.time_step_unscaled ();
-		/// <summary> (Deprecated, use Time.StepUnscaledf) How much time
-		/// elapsed during the last frame? 32 bit time precision. This version
-		/// is unaffected by the Time.Scale value!</summary>
+		/// <summary> (Deprecated, use Time.StepUnscaledf) How many seconds
+		/// have elapsed during the last frame? 32 bit time precision,
+		/// calculated at the start of the frame. This version is unaffected by
+		/// the Time.Scale value!</summary>
 		[Obsolete("Use Time.StepUnscaledf")]
 		public static float  ElapsedUnscaledf => NativeAPI.time_stepf_unscaled();
 
-		/// <summary> How much time has elapsed since the last frame? 64 bit
-		/// time precision.</summary>
+		/// <summary> How many seconds have elapsed since the last frame? 64
+		/// bit time precision, calculated at the start of the frame.</summary>
 		public static double Step => NativeAPI.time_step();
-		/// <summary> How much time has elapsed since the last frame? 32 bit
-		/// time precision.</summary>
+		/// <summary> How many seconds have elapsed since the last frame? 32
+		/// bit time precision, calculated at the start of the frame.</summary>
 		public static float  Stepf => NativeAPI.time_stepf();
-		/// <summary> How much time has elapsed since the last frame? 64 bit
-		/// time precision. This version is unaffected by the Time.Scale value!
-		/// </summary>
+		/// <summary> How many seconds have elapsed since the last frame? 64
+		/// bit time precision, calculated at the start of the frame. This
+		/// version is unaffected by the Time.Scale value! </summary>
 		public static double StepUnscaled => NativeAPI.time_step_unscaled();
-		/// <summary> How much time has elapsed since the last frame? 32 bit
-		/// time precision. This version is unaffected by the Time.Scale value!
-		/// </summary>
+		/// <summary> How many seconds have elapsed since the last frame? 32
+		/// bit time precision, calculated at the start of the frame. This
+		/// version is unaffected by the Time.Scale value! </summary>
 		public static float  StepUnscaledf => NativeAPI.time_stepf_unscaled();
 
 		/// <summary>Time is scaled by this value! Want time to pass slower?
