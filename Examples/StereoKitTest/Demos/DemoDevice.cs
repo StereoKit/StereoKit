6970e4e89964086184376e6acb09dd953510e064
Nick Klingensmith <programmerpichu@gmail.com>
2023-03-30 17:31:32 -0700

    Add origin mode to the Device demo.

--- Examples/StereoKitTest/Demos/DemoDevice.cs
+++ Examples/StereoKitTest/Demos/DemoDevice.cs
@@ -24,6 +24,8 @@ class DemoDevice : ITest
 		UI.Label(Device.DisplayType.ToString(), false);
 		UI.Label("Blend", labelSize); UI.SameLine();
 		UI.Label(Device.DisplayBlend.ToString(), false);
+		UI.Label("Origin", labelSize); UI.SameLine();
+		UI.Label($"{Device.OriginMode} {Device.OriginOffset}", false);
 
 		UI.HSeparator();
 
