fe8f3fa7d169cb86633485f23925974ab5a4e06a
Nick Klingensmith <quic_nklingen@quicinc.com>
2023-07-10 18:46:06 -0700

    Updated a URL in the docs.

--- StereoKitC/stereokit.h
+++ StereoKitC/stereokit.h
@@ -951,7 +951,7 @@ typedef enum tex_format_ {
 /*How does the shader grab pixels from the texture? Or more
   specifically, how does the shader grab colors between the provided
   pixels? If you'd like an in-depth explanation of these topics, check
-  out [this exploration of texture filtering](https://medium.com/@bgolus/sharper-mipmapping-using-shader-based-supersampling-ed7aadb47bec)
+  out [this exploration of texture filtering](https://bgolus.medium.com/sharper-mipmapping-using-shader-based-supersampling-ed7aadb47bec)
   by graphics wizard Ben Golus.*/
 typedef enum tex_sample_ {
 	/*Use a linear blend between adjacent pixels, this creates
