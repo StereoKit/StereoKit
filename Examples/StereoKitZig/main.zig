941db50f99ca8cbf81317de6c74e441e3e2b4f5a
Nick Klingensmith <quic_nklingen@quicinc.com>
2023-07-20 09:32:02 -0700

    Removed some leftovers.

--- Examples/StereoKitZig/main.zig
+++ Examples/StereoKitZig/main.zig
@@ -1,11 +1,9 @@
-const std = @import("std");
 const sk = @import("StereoKit");
 
 var cubeMesh: sk.Mesh = .{};
 var material: sk.Material = .{};
 
-pub fn main() !void {
-    std.debug.print("bool size: {}", .{@sizeOf(bool)});
+pub fn main() void {
     _ = sk.init(.{
         .appName = "StereoKit Zig",
     });
@@ -17,5 +15,5 @@ pub fn main() !void {
 }
 
 export fn step() void {
-    cubeMesh.draw(material, sk.Matrix.ts(.{ .x = 0, .y = 0, .z = -0.5 }, .{ .x = 0.1, .y = 0.1, .z = 0.1 }), .{ .r = 1, .g = 1, .b = 1, .a = 1 }, SK.RenderLayer.Layer0);
+    cubeMesh.draw(material, sk.Matrix.ts(.{ .x = 0, .y = 0, .z = -0.5 }, .{ .x = 0.1, .y = 0.1, .z = 0.1 }), .{ .r = 1, .g = 1, .b = 1, .a = 1 }, sk.RenderLayer.Layer0);
 }
