43f6fdce5f825aca7ec9b1c4e180e70a47f0be79
Manasa Ponugoti <quic_ponugoti@quicinc.com>
2024-01-15 23:21:14 -0800

    remove duplicated variable
    Signed-off-by: Manasa Ponugoti <quic_ponugoti@quicinc.com>

--- StereoKitC/platforms/uwp.cpp
+++ StereoKitC/platforms/uwp.cpp
@@ -747,7 +747,7 @@ void platform_msgbox_err(const char *text, const char *header) {
 
 ///////////////////////////////////////////
 
-void platform_iterate_dir(const char*, void*, void (*)(void* callback_data, const char* name, bool file, const int32_t size, const int32_t size)) {
+void platform_iterate_dir(const char*, void*, void (*)(void* callback_data, const char* name, bool file, const int32_t size)) {
 }
 
 }
