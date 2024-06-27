c8527dc9a9352beaead01644972a0c90511feaa6
Nick Klingensmith <8484884+maluoi@users.noreply.github.com>
2024-06-27 09:26:14 -0700

    Merge pull request #1035 from StereoKit/fix/render-list-demo
    On GL, flip the RenderList draw camera.

--- Examples/StereoKitTest/Demos/DemoRenderList.cs
+++ Examples/StereoKitTest/Demos/DemoRenderList.cs
@@ -11,7 +11,7 @@ class DemoRenderList : ITest
 
 	public void Step()
 	{
-		ShowTextInputWindow();
+		ShowRenderListWindow();
 
 		Demo.ShowSummary(title, description, new Bounds(V.XY0(0, -0.19f), V.XYZ(.4f, .5f, 0)));
 	}
@@ -29,10 +29,15 @@ class DemoRenderList : ITest
 
 	Pose winPose = Pose.Identity;
 	bool clear   = false;
-	void ShowTextInputWindow()
+	void ShowRenderListWindow()
 	{
 		Renderer.ClearColor = Color.HSV(0.4f, 0.3f, 0.5f);
-		list.DrawNow(renderTex, Matrix.LookAt(Vec3.AngleXZ(Time.Totalf * 90, 0.2f) * 2.0f, Vec3.Zero, -Vec3.Up), Matrix.Perspective(90, 1, 0.01f, 50), new Rect(0, 0, 1, 1));
+		// OpenGL renders upside-down to rendertargets, so this is a simple fix
+		// for our case here, we just flip the camera upside down.
+		Vec3 up = Backend.Graphics == BackendGraphics.D3D11
+			?  Vec3.Up
+			: -Vec3.Up;
+		list.DrawNow(renderTex, Matrix.LookAt(Vec3.AngleXZ(Time.Totalf * 90, 0.2f) * 2.0f, Vec3.Zero, up), Matrix.Perspective(90, 1, 0.01f, 50), new Rect(0, 0, 1, 1));
 
 		UI.WindowBegin("Render Lists", ref winPose);
 		UI.Label($"Render items: {RenderList.Primary.PrevCount}");
