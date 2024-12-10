493246e4750caff08821d3fca62dea6dba0b49ed
Nick Klingensmith <quic_nklingen@quicinc.com>
2024-12-09 17:55:39 -0800

    Icon rendering sample for RenderList.
    Signed-off-by: Nick Klingensmith <quic_nklingen@quicinc.com>

--- Examples/StereoKitTest/Demos/DemoRenderList.cs
+++ Examples/StereoKitTest/Demos/DemoRenderList.cs
@@ -3,12 +3,11 @@
 class DemoRenderList : ITest
 {
 	string title       = "Render Lists";
-	string description = "";
+	string description = "All draw calls are stored in RenderList.Primary, but you can also store your draw calls to custom RenderLists! This allows you to draw more specific scenes to render textures with greater control, for use as icons, thumbnails, and all sorts of interesting things!";
 
 	Tex        renderTex = Tex.RenderTarget(128, 128);
 	Material   renderMat = Material.Unlit.Copy();
 	RenderList list      = new RenderList();
-	Color oldClear;
 
 	public void Step()
 	{
@@ -25,7 +24,6 @@ class DemoRenderList : ITest
 		list.Add(model, Matrix.R(0,180,0), Color.White);
 
 		renderMat[MatParamName.DiffuseTex] = renderTex;
-		oldClear = Renderer.ClearColor;
 	}
 	public void Shutdown() { }
 
@@ -33,14 +31,12 @@ class DemoRenderList : ITest
 	bool clear   = false;
 	void ShowRenderListWindow()
 	{
-		Renderer.ClearColor = Color.HSV(0.4f, 0.3f, 0.5f);
 		// OpenGL renders upside-down to rendertargets, so this is a simple fix
 		// for our case here, we just flip the camera upside down.
 		Vec3 up = Backend.Graphics == BackendGraphics.D3D11
 			?  Vec3.Up
 			: -Vec3.Up;
-		list.DrawNow(renderTex, Matrix.LookAt(Vec3.AngleXZ(Time.Totalf * 90, 0.2f) * 2.0f, Vec3.Zero, up), Matrix.Perspective(90, 1, 0.01f, 50), new Rect(0, 0, 1, 1));
-		Renderer.ClearColor = oldClear;
+		list.DrawNow(renderTex, Matrix.LookAt(Vec3.AngleXZ(Time.Totalf * 90, 0.2f) * 2.0f, Vec3.Zero, up), Matrix.Perspective(90, 1, 0.01f, 50), Color.HSV(0.4f, 0.3f, 0.5f));
 
 		UI.WindowBegin("Render Lists", ref winPose);
 		UI.Label($"Render items: {RenderList.Primary.PrevCount}");
