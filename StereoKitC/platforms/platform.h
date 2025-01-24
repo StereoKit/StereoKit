057c9a8159c29e4cc93045c75fc701e14eca29f8
Nick Klingensmith <quic_nklingen@quicinc.com>
2025-01-24 12:06:42 -0800

    Nicer stage anchor persistence behavior.
    Anchor file isn't written if no persistent anchors are present.
    Anchor file is deleted if all anchors are removed.
    
    Signed-off-by: Nick Klingensmith <quic_nklingen@quicinc.com>

--- StereoKitC/platforms/platform.h
+++ StereoKitC/platforms/platform.h
@@ -142,6 +142,7 @@ void   platform_set_window        (void *window);
 void   platform_set_window_xam    (void *window);
 
 bool   platform_file_exists       (const char* filename);
+bool   platform_file_delete       (const char* filename);
 char  *platform_working_dir       ();
 void   platform_iterate_dir       (const char *directory_path, void *callback_data, void (*on_item)(void *callback_data, const char *name, const platform_file_attr_t platform_file_attr_t));
 char  *platform_push_path_ref     (char       *path, const char *directory);
