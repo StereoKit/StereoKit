d79a2e67ddcd4c18c2dbc05a99752df108b3cae5
Nick Klingensmith <8484884+maluoi@users.noreply.github.com>
2025-01-24 17:17:10 -0500

    Merge pull request #1147 from StereoKit/fix/sksettings_assetfolder
    SK.Settings is now SK's internal copy of the settings.

--- StereoKit/SK.cs
+++ StereoKit/SK.cs
@@ -17,9 +17,11 @@ namespace StereoKit
 
 		/// <summary>This is a copy of the settings that StereoKit was
 		/// initialized with, so you can refer back to them a little easier.
-		/// These are read only, and keep in mind that some settings are 
-		/// only requests! Check SK.System and other properties for the 
-		/// current state of StereoKit.</summary>
+		/// Some of these values will be different than provided, as StereoKit
+		/// will resolve some default values based on the platform capabilities
+		/// or internal preference. These are read only, and keep in mind that
+		/// some settings are only requests! Check SK.System and other
+		/// properties for the current state of StereoKit.</summary>
 		public static SKSettings Settings { get; private set; }
 		/// <summary>Has StereoKit been successfully initialized already? If
 		/// initialization was attempted and failed, this value will be 
@@ -147,11 +149,13 @@ namespace StereoKit
 				catch { settings.appName = "StereoKit App"; }
 			}
 
-			// DllImport finds the function at the beginning of the function 
-			// call, so this needs to be in a separate function from 
+			// DllImport finds the function at the beginning of the function
+			// call, so this needs to be in a separate function from
 			// NativeLib.LoadDll
 			bool result = NativeAPI.sk_init(settings);
-			Settings = settings;
+			// Get the "resolved" settings from StereoKit, so we pick up some
+			// of the final defaults or calculated settings.
+			Settings = NativeAPI.sk_get_settings();
 
 			// Get system information
 			if (result) { 
