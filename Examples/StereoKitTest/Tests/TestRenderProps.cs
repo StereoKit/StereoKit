e2d44fea6defaa92553302f625baa543ac6c6143
Nick Klingensmith <nklingen@qti.qualcomm.com>
2025-08-26 15:44:12 -0700

    Quick test for new properties
    Signed-off-by: Nick Klingensmith <nklingen@qti.qualcomm.com>

---
+++ Examples/StereoKitTest/Tests/TestRenderProps.cs
@@ -0,0 +1,62 @@
+﻿using StereoKit;
+
+class TestRenderProps : ITest
+{
+	static bool TestClip()
+	{
+		bool result = true;
+		Renderer.GetClip(out float oldNear, out float oldFar);
+		Log.Info($"Default Clip: Near {oldNear}, Far {oldFar}");
+
+		Renderer.SetClip(0.1f, 1000.0f);
+		Renderer.GetClip(out float newNear, out float newFar);
+		if (newNear != 0.1f || newFar != 1000.0f)
+			result = false;
+
+		Renderer.SetClip(oldFar, oldNear);
+		return result;
+	}
+
+	static bool TestFOV()
+	{
+		bool result = true;
+		float oldFOV = Renderer.FOV;
+		Log.Info($"Default FOV: {oldFOV}");
+
+		Renderer.FOV = 45.0f;
+		if (Renderer.FOV != 45.0f)
+			result = false;
+
+		Renderer.FOV = oldFOV;
+		return result;
+	}
+
+	static bool TestOrthoSize()
+	{
+		bool result = true;
+		float oldOrthoSize = Renderer.OrthoSize;
+		Log.Info($"Default OrthoSize: {oldOrthoSize}");
+
+		Renderer.OrthoSize = 0.5f;
+		if (Renderer.OrthoSize != 0.5f)
+			result = false;
+
+		Renderer.OrthoSize = oldOrthoSize;
+		return result;
+	}
+
+	public void Initialize()
+	{
+		Tests.Test(TestClip);
+		Tests.Test(TestFOV);
+		Tests.Test(TestOrthoSize);
+	}
+
+	public void Shutdown()
+	{
+	}
+
+	public void Step()
+	{
+	}
+}
